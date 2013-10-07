fa04ccb1be96b0b4fef526324bd54e440aea95b1
Sam Edwards <CFSworks@gmail.com>
2013-10-06 22:50:59 -0600

    distributed: Remove decoy prepareAvatar() function (DOS-TT used this to load the client certificate).

--- toontown/distributed/ToontownClientRepository.py
+++ toontown/distributed/ToontownClientRepository.py
@@ -86,7 +86,6 @@ class ToontownClientRepository(OTPClientRepository.OTPClientRepository):
         self.toons = {}
         if self.http.getVerifySsl() != HTTPClient.VSNoVerify:
             self.http.setVerifySsl(HTTPClient.VSNoDateCheck)
-        prepareAvatar(self.http)
         self.__forbidCheesyEffects = 0
         self.friendManager = None
         self.speedchatRelay = None
