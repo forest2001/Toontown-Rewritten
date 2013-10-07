forest2001/Toontown-Rewritten

commit 3f0a989ded385932498ca6fcccdb9dbfcd5c6eda

general: For compatibility with Panda3D 1.8.1, don't do string.foo(bar), do bar.foo().

otp/ai/MagicWordManager.py
@@ -146,7 +146,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
         elif wordIs('~avId'):
             self.setMagicWordResponse(str(localAvatar.doId))
         elif wordIs('~doId'):
-            name = string.strip(word[6:])
+            name = word[6:].strip()
             objs = self.identifyDistributedObjects(name)
             if len(objs) == 0:
                 response = '%s is unknown.' % name
@@ -184,13 +184,13 @@ class MagicWordManager(DistributedObject.DistributedObject):
                 self.setMagicWordResponse(response)
             else:
                 tm.extraSkew = 0.0
-                skew = string.strip(word[5:])
+                skew = word[5:].strip()
                 if skew != '':
                     tm.extraSkew = float(skew)
                 globalClockDelta.clear()
                 tm.handleHotkey()
         elif wordIs('~period'):
-            timeout = string.strip(word[7:])
+            timeout = word[7:].strip()
             if timeout != '':
                 seconds = int(timeout)
                 self.cr.stopPeriodTimer()
@@ -618,7 +618,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
                 return
 
         nextWord = word[b + 1:]
-        name = string.strip(word[5:b])
+        name = word[5:b].strip()
         id = self.identifyAvatar(name)
         if id == None:
             self.setMagicWordResponse("Don't know who %s is." % name)
@@ -631,7 +631,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
 
     def identifyDistributedObjects(self, name):
         result = []
-        lowerName = string.lower(name)
+        lowerName = name.lower()
         for obj in self.cr.doId2do.values():
             className = obj.__class__.__name__
             try:
@@ -639,7 +639,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
             except:
                 name = className
 
-            if string.lower(name) == lowerName or string.lower(className) == lowerName or string.lower(className) == 'distributed' + lowerName:
+            if name.lower() == lowerName or className.lower() == lowerName or className.lower() == 'distributed' + lowerName:
                 result.append((name, obj))
 
         return result
@@ -666,7 +666,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
         return
 
     def getCSBitmask(self, str):
-        words = string.lower(str).split()
+        words = str.lower().split()
         if len(words) == 0:
             return None
         invalid = ''
@@ -717,7 +717,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
         return None
 
     def showfont(self, fontname):
-        fontname = string.strip(string.lower(fontname))
+        fontname = string.lower(fontname.strip())
         font = self.getFontByName(fontname)
         if font == None:
             self.setMagicWordResponse('Unknown font: %s' % fontname)
@@ -842,9 +842,9 @@ class MagicWordManager(DistributedObject.DistributedObject):
             if isinstance(av, self.GameAvatarClass) and av.getName() == name:
                 return av.doId
 
-        lowerName = string.lower(name)
+        lowerName = name.lower()
         for av in Avatar.Avatar.ActiveAvatars:
-            if isinstance(av, self.GameAvatarClass) and string.strip(string.lower(av.getName())) == lowerName:
+            if isinstance(av, self.GameAvatarClass) and string.lower(av.getName(.strip())) == lowerName:
                 return av.doId
 
         try:

otp/chat/ChatGlobals.py
@@ -40,7 +40,7 @@ ThoughtPrefix = '.'
 def isThought(message):
     if len(message) == 0:
         return 0
-    elif string.find(message, ThoughtPrefix, 0, len(ThoughtPrefix)) >= 0:
+    elif message.find(ThoughtPrefix, 0, len(ThoughtPrefix)) >= 0:
         return 1
     else:
         return 0

otp/chat/ChatManager.py
@@ -24,7 +24,7 @@ ThoughtPrefix = '.'
 def isThought(message):
     if len(message) == 0:
         return 0
-    elif string.find(message, ThoughtPrefix, 0, len(ThoughtPrefix)) >= 0:
+    elif message.find(ThoughtPrefix, 0, len(ThoughtPrefix)) >= 0:
         return 1
     else:
         return 0

otp/chat/TalkAssistant.py
@@ -208,7 +208,7 @@ class TalkAssistant(DirectObject.DirectObject):
             return 0
         elif len(message) == 0:
             return 0
-        elif string.find(message, ThoughtPrefix, 0, len(ThoughtPrefix)) >= 0:
+        elif message.find(ThoughtPrefix, 0, len(ThoughtPrefix)) >= 0:
             return 1
         else:
             return 0

otp/distributed/OTPClientRepository.py
@@ -513,9 +513,9 @@ class OTPClientRepository(ClientRepositoryBase):
             if downloadParFilename.exists():
                 downloadPar = open(downloadParFilename.toOsSpecific())
                 for line in downloadPar.readlines():
-                    i = string.find(line, 'VALIDATE_DOWNLOAD=')
+                    i = line.find('VALIDATE_DOWNLOAD=')
                     if i != -1:
-                        self.validateDownload = string.strip(line[i + 18:])
+                        self.validateDownload = line[i + 18:].strip()
                         break
 
     def getServerVersion(self):

otp/friends/FriendSecret.py
@@ -448,7 +448,7 @@ class FriendSecret(DirectFrame, StateData.StateData):
 
     def __enterSecret(self, secret):
         self.enterSecret.set('')
-        secret = string.strip(secret)
+        secret = secret.strip()
         if not secret:
             self.exit()
             return

otp/launcher/LauncherBase.py
@@ -188,7 +188,7 @@ class LauncherBase(DirectObject):
             downloadServerString = self.getValue('DOWNLOAD_SERVER', '')
         self.notify.info('Download Server List %s' % downloadServerString)
         self.downloadServerList = []
-        for name in string.split(downloadServerString, ';'):
+        for name in downloadServerString.split(';'):
             url = URLSpec(name, 1)
             self.downloadServerList.append(url)
 
@@ -921,7 +921,7 @@ class LauncherBase(DirectObject):
             if self.linesInProgress[i].find(phase) > -1:
                 nameSizeTuple = self.linesInProgress[i].split()
                 numSize = nameSizeTuple[1].split('L')
-                sum += string.atoi(numSize[0])
+                sum += numSize[0].atoi()
 
         return sum
 

otp/level/Entity.py
@@ -60,7 +60,7 @@ class Entity(DirectObject):
         return self.getZoneEntity().getNodePath()
 
     def privGetSetter(self, attrib):
-        setFuncName = 'set%s%s' % (string.upper(attrib[0]), attrib[1:])
+        setFuncName = 'set%s%s' % (attrib[0].upper(), attrib[1:])
         if hasattr(self, setFuncName):
             return getattr(self, setFuncName)
         return None

commit 91e1c20a7faf2df5f7b668bbbadd032319786b06

cleanup: Fix mistakes in otp.ai.MagicWordManager introduced by 3f0a989.

otp/ai/MagicWordManager.py
@@ -717,7 +717,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
         return None
 
     def showfont(self, fontname):
-        fontname = string.lower(fontname.strip())
+        fontname = fontname.lower().strip()
         font = self.getFontByName(fontname)
         if font == None:
             self.setMagicWordResponse('Unknown font: %s' % fontname)
@@ -844,7 +844,7 @@ class MagicWordManager(DistributedObject.DistributedObject):
 
         lowerName = name.lower()
         for av in Avatar.Avatar.ActiveAvatars:
-            if isinstance(av, self.GameAvatarClass) and string.lower(av.getName(.strip())) == lowerName:
+            if isinstance(av, self.GameAvatarClass) and av.getName().lower().strip() == lowerName:
                 return av.doId
 
         try: