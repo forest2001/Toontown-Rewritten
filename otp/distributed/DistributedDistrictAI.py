dc1d136e7dc72edd0d95f89ccc10340d0a6c6ca7
Sam Edwards <CFSworks@gmail.com>
2013-10-09 20:47:35 -0600

    distributed: Fill out the AI district stubs.

--- otp/distributed/DistributedDistrictAI.py
+++ otp/distributed/DistributedDistrictAI.py
@@ -3,10 +3,32 @@ from direct.distributed.DistributedObjectAI import DistributedObjectAI
 
 class DistributedDistrictAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedDistrictAI")
+    name = 'District'
+    available = 0
 
-    def setName(self, todo0):
-        pass
+    def setName(self, name):
+        self.name = name
 
-    def setAvailable(self, todo0):
-        pass
+    def d_setName(self, name):
+        self.sendUpdate('setName', [name])
+
+    def b_setName(self, name):
+        self.setName(name)
+        self.d_setName(name)
+
+    def getName(self):
+        return self.name
+
+    def setAvailable(self, available):
+        self.available = available
+
+    def d_setAvailable(self, available):
+        self.sendUpdate('setAvailable', [available])
+
+    def b_setAvailable(self, available):
+        self.setAvailable(available)
+        self.d_setAvailable(available)
+
+    def getAvailable(self):
+        return self.available
 
--- toontown/distributed/ToontownDistrictAI.py
+++ toontown/distributed/ToontownDistrictAI.py
@@ -3,10 +3,17 @@ from otp.distributed.DistributedDistrictAI import DistributedDistrictAI
 
 class ToontownDistrictAI(DistributedDistrictAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("ToontownDistrictAI")
+    ahnnLog = 0
 
-    def setParentingRules(self, todo0, todo1):
-        pass
+    def allowAHNNLog(self, ahnnLog):
+        self.ahnnLog = ahnnLog
 
-    def allowAHNNLog(self, todo0):
-        pass
+    def d_allowAHNNLog(self, ahnnLog):
+        self.sendUpdate('allowAHNNLog', [ahnnLog])
 
+    def b_allowAHNNLog(self, ahnnLog):
+        self.allowAHNNLog(ahnnLog)
+        self.d_allowAHNNLog(ahnnLog)
+
+    def getAllowAHNNLog(self):
+        return self.ahnnLog
