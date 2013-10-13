029316c72f6f0a87e75dbc002f56a40027006844
Sam Edwards <CFSworks@gmail.com>
2013-10-13 05:08:24 -0600

    nametag: Add getNametag2d() and setActive() to NametagGroup.

---
+++ otp/nametag/Nametag2d.py
@@ -0,0 +1,6 @@
+from Nametag import *
+from pandac.PandaModules import *
+
+class Nametag2d(Nametag, PandaNode):
+    def setContents(self, contents):
+        pass
--- otp/nametag/NametagGroup.py
+++ otp/nametag/NametagGroup.py
@@ -1,5 +1,6 @@
 from pandac.PandaModules import *
 from Nametag3d import *
+from Nametag2d import *
 
 class NametagGroup:
     CCNormal = 0
@@ -13,15 +14,21 @@ class NametagGroup:
     CCFreeChat = 8
 
     def __init__(self):
-        self.nametag2d = None
+        self.nametag2d = Nametag2d('')
         self.nametag3d = Nametag3d('')
         self.icon = PandaNode('icon')
+
+    def getNametag2d(self):
+        return self.nametag2d
 
     def getNametag3d(self):
         return self.nametag3d
 
     def getNameIcon(self):
         return self.icon
+
+    def setActive(self, active):
+        pass
 
     def setAvatar(self, avatar):
         pass
