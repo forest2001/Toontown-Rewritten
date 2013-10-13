f565f488514088fdfa8219d704323b08ba31e195
Sam Edwards <CFSworks@gmail.com>
2013-10-13 04:34:12 -0600

    uberdog: Update CSM with support for retrieving avatars lists.

--- toontown/uberdog/ClientServicesManager.py
+++ toontown/uberdog/ClientServicesManager.py
@@ -1,9 +1,11 @@
 from direct.distributed.DistributedObjectGlobal import DistributedObjectGlobal
 from direct.directnotify.DirectNotifyGlobal import directNotify
+from otp.distributed.PotentialAvatar import PotentialAvatar
 
 class ClientServicesManager(DistributedObjectGlobal):
     notify = directNotify.newCategory('ClientServicesManager')
 
+    # --- LOGIN LOGIC ---
     def performLogin(self, doneEvent):
         self.doneEvent = doneEvent
 
@@ -14,3 +16,16 @@ class ClientServicesManager(DistributedObjectGlobal):
 
     def acceptLogin(self):
         messenger.send(self.doneEvent, [{'mode': 'success'}])
+
+
+    # --- AVATARS LIST ---
+    def requestAvatars(self):
+        self.sendUpdate('requestAvatars')
+
+    def setAvatars(self, avatars):
+        avList = []
+        for avNum, avName, avDNA, avPosition, aname in avatars:
+            names = [avName, '', '', '']
+            avList.append(PotentialAvatar(avNum, names, avDNA, avPosition, aname))
+
+        self.cr.handleAvatarsList(avList)
--- toontown/uberdog/ClientServicesManagerUD.py
+++ toontown/uberdog/ClientServicesManagerUD.py
@@ -1,11 +1,29 @@
 from direct.distributed.DistributedObjectGlobalUD import DistributedObjectGlobalUD
 from direct.directnotify.DirectNotifyGlobal import directNotify
+from direct.distributed.PyDatagram import *
 
 class ClientServicesManagerUD(DistributedObjectGlobalUD):
     notify = directNotify.newCategory('ClientServicesManagerUD')
 
     def login(self, cookie):
-        print cookie
+        self.notify.debug('Received login cookie %r from %d' % (cookie, self.air.getMsgSender()))
 
-    def acceptLogin(self):
-        messenger.send(self.doneEvent)
+        pdg = PyDatagram()
+        pdg.addServerHeader(self.air.getMsgSender(), self.air.ourChannel, 3110)
+        pdg.addUint16(2)
+        self.air.send(pdg)
+
+        self.sendUpdateToChannel(self.air.getMsgSender(), 'acceptLogin', [])
+
+    def requestAvatars(self):
+        self.notify.debug('Received avatar list request from %d' % (self.air.getMsgSender()))
+
+        av = [123,
+              'Shockley',
+              't\x05\x01\x01\x01\x01\t\x01\t\x00\r\x1a\x00\x1a\x1a',
+              1,
+              0]
+
+        avs = [av]
+
+        self.sendUpdateToChannel(self.air.getMsgSender(), 'setAvatars', [avs])
