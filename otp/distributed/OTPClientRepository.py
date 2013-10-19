e22111d7106602fc2c35aef9423dc163df64f1dd
Sam Edwards <CFSworks@gmail.com>
2013-10-18 18:01:56 -0600

    distributed: Properly handle unexpected avatar deletion, display "district reset" message.

--- otp/distributed/OTPClientRepository.py
+++ otp/distributed/OTPClientRepository.py
@@ -2015,6 +2015,16 @@ class OTPClientRepository(ClientRepositoryBase):
         if not self.isLocalId(doId):
             self.disableDoId(doId, ownerView)
 
+        if getattr(base, 'localAvatar', None) and doId == base.localAvatar.doId:
+            self.bootedIndex = 153
+            self.bootedText = ''
+
+            self.notify.warning("Avatar deleted! Closing connection...")
+
+            # disconnect now, don't wait for send/recv to fail
+            self.stopReaderPollTask()
+            self.lostConnection()
+
     def sendSetLocation(self, doId, parentId, zoneId):
         datagram = PyDatagram()
         datagram.addUint16(CLIENT_OBJECT_LOCATION)
