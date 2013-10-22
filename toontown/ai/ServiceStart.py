19b8afcb143dd925fb908b39dd27de0c37ca7d1d
Sam Edwards <CFSworks@gmail.com>
2013-10-21 19:11:21 -0600

    ai,uberdog: Accept command-line arguments to ServiceStart.

--- toontown/ai/ServiceStart.py
+++ toontown/ai/ServiceStart.py
@@ -1,7 +1,28 @@
 from pandac.PandaModules import *
 
-# Dev stuff that should probably be removed before deployment:
-loadPrcFile('config/dev.prc')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--base-channel', help='The base channel that the server may use.')
+parser.add_argument('--max-channels', help='The number of channels the server may use.')
+parser.add_argument('--stateserver', help="The control channel of this AI's designated State Server.")
+parser.add_argument('--district-name', help="What this AI Server's district will be named.")
+parser.add_argument('--astron-ip', help="The IP address of the Astron Message Director to connect to.")
+parser.add_argument('--eventlogger-ip', help="The IP address of the Astron Event Logger to log to.")
+parser.add_argument('config', nargs='?', default=['config/dev.prc'], help="PRC file(s) to load.")
+args = parser.parse_args()
+
+for prc in args.config:
+    loadPrcFile(prc)
+
+localconfig = ''
+if args.base_channel: localconfig += 'air-base-channel %s\n' % args.base_channel
+if args.max_channels: localconfig += 'air-channel-allocation %s\n' % args.max_channels
+if args.stateserver: localconfig += 'air-stateserver %s\n' % args.stateserver
+if args.district_name: localconfig += 'district-name %s\n' % args.district_name
+if args.astron_ip: localconfig += 'air-connect %s\n' % args.astron_ip
+if args.eventlogger_ip: localconfig += 'eventlog-host %s\n' % args.eventlogger_ip
+loadPrcFileData('Command-line', localconfig)
 
 class game:
     name = 'toontown'
@@ -11,7 +32,14 @@ __builtins__.game = game
 from otp.ai.AIBaseGlobal import *
 
 from toontown.ai.ToontownAIRepository import ToontownAIRepository
-simbase.air = ToontownAIRepository(401000000, 10000, 'Devhaven')
-simbase.air.connect('127.0.0.1')
+simbase.air = ToontownAIRepository(config.GetInt('air-base-channel', 401000000),
+                                   config.GetInt('air-stateserver', 10000),
+                                   config.GetString('district-name', 'Devhaven'))
+host = config.GetString('air-connect', '127.0.0.1')
+port = 7199
+if ':' in host:
+    host, port = host.split(':', 1)
+    port = int(port)
+simbase.air.connect(host, port)
 
 run()
--- toontown/uberdog/ServiceStart.py
+++ toontown/uberdog/ServiceStart.py
@@ -1,7 +1,26 @@
 from pandac.PandaModules import *
 
-# Dev stuff that should probably be removed before deployment:
-loadPrcFile('config/dev.prc')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--base-channel', help='The base channel that the server may use.')
+parser.add_argument('--max-channels', help='The number of channels the server may use.')
+parser.add_argument('--stateserver', help="The control channel of this UD's designated State Server.")
+parser.add_argument('--astron-ip', help="The IP address of the Astron Message Director to connect to.")
+parser.add_argument('--eventlogger-ip', help="The IP address of the Astron Event Logger to log to.")
+parser.add_argument('config', nargs='?', default=['config/dev.prc'], help="PRC file(s) to load.")
+args = parser.parse_args()
+
+for prc in args.config:
+    loadPrcFile(prc)
+
+localconfig = ''
+if args.base_channel: localconfig += 'air-base-channel %s\n' % args.base_channel
+if args.max_channels: localconfig += 'air-channel-allocation %s\n' % args.max_channels
+if args.stateserver: localconfig += 'air-stateserver %s\n' % args.stateserver
+if args.astron_ip: localconfig += 'air-connect %s\n' % args.astron_ip
+if args.eventlogger_ip: localconfig += 'eventlog-host %s\n' % args.eventlogger_ip
+loadPrcFileData('Command-line', localconfig)
 
 class game:
     name = 'uberDog'
@@ -11,7 +30,13 @@ __builtins__.game = game
 from otp.ai.AIBaseGlobal import *
 
 from toontown.uberdog.ToontownUberRepository import ToontownUberRepository
-simbase.air = ToontownUberRepository(400000000, 10000)
-simbase.air.connect('127.0.0.1')
+simbase.air = ToontownUberRepository(config.GetInt('air-base-channel', 400000000),
+                                     config.GetInt('air-stateserver', 10000))
+host = config.GetString('air-connect', '127.0.0.1')
+port = 7199
+if ':' in host:
+    host, port = host.split(':', 1)
+    port = int(port)
+simbase.air.connect(host, port)
 
 run()
