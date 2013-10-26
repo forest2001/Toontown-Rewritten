2a5f3f7676484b716aed58c3c2234c08df119357
Sam Edwards <CFSworks@gmail.com>
2013-10-26 17:00:34 -0600

    nametag: Billboard 3d nametags.

--- otp/nametag/Nametag3d.py
+++ otp/nametag/Nametag3d.py
@@ -2,9 +2,10 @@ from Nametag import *
 import NametagGlobals
 from pandac.PandaModules import *
 
-class Nametag3d(Nametag, PandaNode):
+class Nametag3d(Nametag):
     def __init__(self):
         Nametag.__init__(self)
+        self.setEffect(BillboardEffect.makeAxis())
 
     def showSpeech(self):
         bubble = NametagGlobals.speechBalloon3d.generate(self.chatString, self.font)
