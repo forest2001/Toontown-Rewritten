2e3f43b8310b08431ddefde5a6715ced105ff031
Sam Edwards <CFSworks@gmail.com>
2013-10-26 16:16:57 -0600

    nametag: Uncomment nametag import.

--- toontown/suit/DistributedBossbotBoss.py
+++ toontown/suit/DistributedBossbotBoss.py
@@ -2,7 +2,7 @@ import math
 import random
 from pandac.PandaModules import VBase3, CollisionPlane, CollisionNode, CollisionSphere, CollisionTube, NodePath, Plane, Vec3, Vec2, Point3, BitMask32, CollisionHandlerEvent, TextureStage, VBase4, BoundingSphere
 from otp.nametag.NametagConstants import CFSpeech
-#from otp.nametag.NametagGroup import NametagGroup # TODO -- NAMETAGTODO
+from otp.nametag.NametagGroup import NametagGroup
 from direct.interval.IntervalGlobal import Sequence, Wait, Func, LerpHprInterval, Parallel, LerpPosInterval, Track, ActorInterval, ParallelEndTogether, LerpFunctionInterval, LerpScaleInterval, LerpPosHprInterval, SoundInterval
 from direct.task import Task
 from direct.fsm import FSM
