339eabb61846922a0de329b2db50d2ab9ba09e7d
Sam Edwards <CFSworks@gmail.com>
2013-10-26 19:19:43 -0600

    nametag: Sanely scale the contents of 3D nametags.

--- otp/nametag/Nametag3d.py
+++ otp/nametag/Nametag3d.py
@@ -3,9 +3,12 @@ import NametagGlobals
 from pandac.PandaModules import *
 
 class Nametag3d(Nametag):
+    CONTENTS_SCALE = 0.33
+
     def __init__(self):
         Nametag.__init__(self)
         self.innerNP.setBillboardAxis()
+        self.innerNP.setScale(self.CONTENTS_SCALE)
 
     def showSpeech(self):
         bubble = NametagGlobals.speechBalloon3d.generate(self.chatString, self.font)
