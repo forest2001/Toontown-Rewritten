d74ed1b508987ed96e2e6a4ba76210b44b93d90c
Harvir <harvir14@gmail.com>
2013-11-03 00:58:15 +0000

    minigame: Update Ice Slide's scoreboard to be widescreen-friendly.

--- toontown/minigame/DistributedIceGame.py
+++ toontown/minigame/DistributedIceGame.py
@@ -284,7 +284,8 @@ class DistributedIceGame(DistributedMinigame.DistributedMinigame, DistributedIce
             avName = self.getAvatarName(avId)
             scorePanel = MinigameAvatarScorePanel.MinigameAvatarScorePanel(avId, avName)
             scorePanel.setScale(0.9)
-            scorePanel.setPos(0.75 - spacing * (self.numPlayers - 1 - i), 0.0, 0.875)
+            scorePanel.setPos(-0.583 - spacing * (self.numPlayers - 1 - i), 0.0, -0.15)
+            scorePanel.reparentTo(base.a2dTopRight)
             scorePanel.makeTransparent(0.75)
             self.scorePanels.append(scorePanel)
 
