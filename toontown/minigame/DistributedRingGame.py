868c118ac47222d30f31874b3e51623c80bd0630
Joey Z <joey1998212@gmail.com>
2013-11-03 16:41:30 -0500

    Start music at correct time for Ring Game

--- toontown/minigame/DistributedRingGame.py
+++ toontown/minigame/DistributedRingGame.py
@@ -173,7 +173,6 @@ class DistributedRingGame(DistributedMinigame):
         self.__spawnUpdateEnvironTask()
         self.__spawnUpdateShadowsTask()
         self.__spawnUpdateLocalToonTask()
-        base.playMusic(self.music, looping=0, volume=0.8)
         if None != self.sndAmbience:
             base.playSfx(self.sndAmbience, looping=1, volume=0.8)
         return
@@ -284,6 +283,7 @@ class DistributedRingGame(DistributedMinigame):
         self.notify.debug('setGameStart')
         DistributedMinigame.setGameStart(self, timestamp)
         self.gameFSM.request('swim')
+        base.playMusic(self.music, looping=0, volume=0.8)
 
     def enterOff(self):
         self.notify.debug('enterOff')
