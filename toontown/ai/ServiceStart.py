a98719e04a695a29f4fbf8b0c79b940f8506b2fd
Sam Edwards <CFSworks@gmail.com>
2013-11-06 02:48:41 -0700

    services: Log crash exceptions to Event Logger.

--- toontown/ai/ServiceStart.py
+++ toontown/ai/ServiceStart.py
@@ -1,4 +1,5 @@
 from pandac.PandaModules import *
+from direct.showbase import PythonUtil
 
 import argparse
 
@@ -42,4 +43,11 @@ if ':' in host:
     port = int(port)
 simbase.air.connect(host, port)
 
-run()
+try:
+    run()
+except SystemExit:
+    raise
+except Exception:
+    info = PythonUtil.describeException()
+    simbase.air.writeServerEvent('ai-exception', simbase.air.getAvatarIdFromSender(), simbase.air.getAccountIdFromSender(), info)
+    raise
