f3ab73a0a48d4bd81899bba6ae1a0d9a1d11e3e5
Jeremy Koletar <jjkoletar@gmail.com>
2013-11-09 21:45:27 -0600

    magic words: access 200 is A-OK for magic word usage, change <= to <

--- otp/ai/MagicWordManagerAI.py
+++ otp/ai/MagicWordManagerAI.py
@@ -15,7 +15,7 @@ class MagicWordManagerAI(DistributedObjectAI):
             self.sendUpdateToAvatarId(invokerId, 'sendMagicWordResponse', ['missing invoker'])
             return
 
-        if invoker.getAdminAccess() <= MINIMUM_MAGICWORD_ACCESS:
+        if invoker.getAdminAccess() < MINIMUM_MAGICWORD_ACCESS:
             self.air.writeServerEvent('suspicious', invokerId, 'Attempted to issue magic word: %s' % word)
             dg = PyDatagram()
             dg.addServerHeader(self.GetPuppetConnectionChannel(invokerId), self.air.ourChannel, CLIENTAGENT_EJECT)
