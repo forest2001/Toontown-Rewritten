157f3a416fc9ed44764426df0a3e07dff74c5fd8
Sam Edwards <CFSworks@gmail.com>
2013-11-16 16:54:00 -0700

    dna: Move lexer rules to separate definitions as well.

--- toontown/dna/DNAParser.py
+++ toontown/dna/DNAParser.py
@@ -73,35 +73,37 @@ t_ignore = ' \t'
 literals = '[],'
 
 def t_ignore_COMMENT(t):
-    r'[/]{2,2}.*'
+    pass
+t_ignore_COMMENT.__doc__ = r'[/]{2,2}.*'
 
 def t_ignore_ML_COMMENT(t):
-    r'\/\*([^*]|[\r\n])*\*/'
+    pass
+t_ignore_ML_COMMENT.__doc__ = r'\/\*([^*]|[\r\n])*\*/'
 
 def t_QUOTED_STRING(t):
-    r'["][^"]*["]'
     t.value = t.value[1:-1]
     return t
+t_QUOTED_STRING.__doc__ = r'["][^"]*["]'
 
 def t_FLOAT(t):
-    r'[+-]?\d+[.]\d*([e][+-]\d+)?'
     t.value = float(t.value)
     return t
+t_FLOAT.__doc__ = r'[+-]?\d+[.]\d*([e][+-]\d+)?'
 
 def t_INTEGER(t):
-    r'[+-]?\d+'
     t.value = int(t.value)
     return t
+t_INTEGER.__doc__ = r'[+-]?\d+'
 
 def t_UNQUOTED_STRING(t):
-    r'[^ \t\n\r\[\],"]+'
     if t.value in reserved:
         t.type = reserved[t.value]
     return t
+t_UNQUOTED_STRING.__doc__ = r'[^ \t\n\r\[\],"]+'
 
 def t_newline(t):
-    r'\n+'
     t.lexer.lineno += len(t.value)
+t_newline.__doc__ = r'\n+'
 
 def t_error(t):
     print 'Illegal character %s' % t.value[0]
