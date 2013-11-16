e7ac6d6d93eda28fe57e69d8fac91d2c185bd556
Aidan Noll <aidan.noll@gmail.com>
2013-11-16 16:13:47 -0500

    hood: hackfix to put fishing pond in donald's dock

--- toontown/hood/DDHoodAI.py
+++ toontown/hood/DDHoodAI.py
@@ -12,6 +12,15 @@ class DDHoodAI(HoodAI):
         HoodAI.__init__(self, air)
         
         self.createTrolley()
+        
+        #TODO: make this better
+        self.createPond()
+        self.createSpot(-1.79822, 139.984, 3.59855, 135, 0, 0)
+        self.createSpot(-11.6229, 148.498, 3.64751, 165, 0, 0)
+        self.createSpot(-23.6427, 149.15, 3.59725, -165, 0, 0)
+        self.createSpot(-31.3754, 141.368, 3.56653, -135, 0, 0)
+        
+        NPCToons.createNPC(air, 1008, NPCToons.NPCToonDict.get(1008), 1000, posIndex=0)
 
         self.boat = DistributedBoatAI(self.air)
         self.boat.generateWithRequired(self.safezone)
