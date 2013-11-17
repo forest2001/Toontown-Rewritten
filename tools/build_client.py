90f548cb9c111f94471ca427f44db87ae7c26b62
Sam Edwards <CFSworks@gmail.com>
2013-11-16 18:34:28 -0700

    tools: Bundle animated prop code into client.

--- tools/build_client.py
+++ tools/build_client.py
@@ -19,6 +19,27 @@ EXTRA_MODULES = (
   'encodings.ascii',
   'encodings.latin_1',
   '_strptime',
+
+  # Animated props and things loaded through DNA...
+  'toontown.building.DistributedAnimatedProp'
+  'toontown.hood.MailboxZeroAnimatedProp'
+  'toontown.hood.PetShopFishAnimatedProp'
+  'toontown.hood.TrashcanOneAnimatedProp'
+  'toontown.hood.SleepingHydrantAnimatedProp'
+  'toontown.hood.HQPeriscopeAnimatedProp'
+  'toontown.hood.HydrantTwoAnimatedProp'
+  'toontown.hood.HydrantZeroAnimatedProp'
+  'toontown.hood.AnimatedProp'
+  'toontown.hood.HQTelescopeAnimatedProp'
+  'toontown.hood.MailboxTwoAnimatedProp'
+  'toontown.hood.GenericAnimatedProp'
+  'toontown.hood.FishAnimatedProp'
+  'toontown.hood.HydrantOneAnimatedProp'
+  'toontown.hood.TrashcanZeroAnimatedProp'
+  'toontown.hood.TrashcanTwoAnimatedProp'
+  'toontown.hood.InteractiveAnimatedProp'
+  'toontown.hood.MailboxOneAnimatedProp'
+  'toontown.hood.ZeroAnimatedProp'
 )
 
 root = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
