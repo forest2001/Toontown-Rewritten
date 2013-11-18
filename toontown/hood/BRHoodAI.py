9628de768be46fc002385992f222335032cbdc1f
Harvir <harvir14@gmail.com>
2013-11-18 22:03:40 +0000

    SZ: Add The Brrrgh.

---
+++ toontown/hood/BRHoodAI.py
@@ -0,0 +1,8 @@
+from toontown.toonbase import ToontownGlobals
+from HoodAI import HoodAI
+
+class BRHoodAI(HoodAI):
+    HOOD = ToontownGlobals.TheBrrrgh
+
+    def createSafeZone(self):
+        HoodAI.createSafeZone(self)
