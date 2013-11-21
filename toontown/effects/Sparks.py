2326eb8741e1394eb6c1f97525e03800434cca78
Aidan <aidan.noll@gmail.com>
2013-11-20 21:36:54 -0500

    effect: make Drift and Sparks use new particle system

--- toontown/effects/Sparks.py
+++ toontown/effects/Sparks.py
@@ -1,8 +1,7 @@
 from pandac.PandaModules import *
 from direct.particles import ParticleEffect
 from direct.directnotify import DirectNotifyGlobal
-from direct.showbase import AppRunnerGlobal
-import os
+from toontown.battle import BattleParticles
 
 class Sparks(NodePath):
 
@@ -13,28 +12,7 @@ class Sparks(NodePath):
         self.renderParent.setBin('fixed', 0)
         self.renderParent.setDepthWrite(0)
         self.assign(parent.attachNewNode('sparks'))
-        self.effect = ParticleEffect.ParticleEffect('Sparks')
-        particleSearchPath = DSearchPath()
-        if AppRunnerGlobal.appRunner:
-            particleSearchPath.appendDirectory(Filename.expandFrom('$TT_3_5_ROOT/phase_3.5/etc'))
-        else:
-            basePath = os.path.expandvars('$TOONTOWN') or './toontown'
-            particleSearchPath.appendDirectory(Filename.fromOsSpecific(basePath + '/src/effects'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_3.5/etc'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_4/etc'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_5/etc'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_6/etc'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_7/etc'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_8/etc'))
-            particleSearchPath.appendDirectory(Filename('resources/phase_9/etc'))
-            particleSearchPath.appendDirectory(Filename('.'))
-        pfile = Filename('sparks.ptf')
-        found = vfs.resolveFilename(pfile, particleSearchPath)
-        if not found:
-            notify.warning('loadParticleFile() - no path: %s' % pfile)
-            return
-        notify.debug('Loading particle file: %s' % pfile)
-        self.effect.loadConfig(pfile)
+        self.effect = BattleParticles.loadParticleFile('sparks.ptf')
         ren = self.effect.getParticlesNamed('particles-1').getRenderer()
         ren.setTextureFromNode('phase_6/models/karting/particleSpark', '**/*')
 
