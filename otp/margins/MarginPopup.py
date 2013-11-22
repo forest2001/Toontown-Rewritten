dcce86e43d03f2f047a1ed374dd5b50523193da2
Sam Edwards <CFSworks@gmail.com>
2013-11-22 13:37:18 -0700

    margins: Implement (most of) the margin system.

--- otp/margins/MarginPopup.py
+++ otp/margins/MarginPopup.py
@@ -1,14 +1,38 @@
 from pandac.PandaModules import *
 
-class MarginPopup(PandaNode):
+class MarginPopup:
     def __init__(self):
-        PandaNode.__init__(self, '')
+        self.__manager = None
+        self.__visible = False
+
+        # The margin management system uses these:
+        self._assignedCell = None
+        self._lastCell = None
 
     def setVisible(self, visibility):
-        pass
+        visibility = bool(visibility)
+        if self.__visible == visibility: return
+
+        self.__visible = visibility
+
+        if self.__manager is not None:
+            if visibility:
+                self.__manager.addVisiblePopup(self)
+            else:
+                self.__manager.removeVisiblePopup(self)
+
+    def getPriority(self):
+        return 0
 
     def manage(self, manager):
-        pass
+        self.unmanage(self.__manager)
+        self.__manager = manager
+
+        if self.__visible:
+            manager.addVisiblePopup(self)
 
     def unmanage(self, manager):
-        pass
+        if self.__manager is not None:
+            if self.__visible:
+                self.__manager.removeVisiblePopup(self)
+            self.__manager = None
