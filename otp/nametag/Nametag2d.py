1281609bf7c368e5c71d9f996f5d25d468ee94d2
Sam Edwards <CFSworks@gmail.com>
2013-11-22 14:12:48 -0700

    nametag: Scale 2D nametags sanely.

--- otp/nametag/Nametag2d.py
+++ otp/nametag/Nametag2d.py
@@ -3,11 +3,15 @@ from otp.margins.MarginPopup import *
 from pandac.PandaModules import *
 
 class Nametag2d(Nametag, MarginPopup):
+    SCALE_2D = 0.3
+
     def __init__(self):
         Nametag.__init__(self)
         MarginPopup.__init__(self)
 
         self.contents = self.CName|self.CSpeech
+
+        self.innerNP.setScale(self.SCALE_2D)
 
     def showBalloon(self, balloon, text):
         text = '%s: %s' % (self.displayName, text)
