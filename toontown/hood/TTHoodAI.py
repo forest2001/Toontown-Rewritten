0a12ee032d83fcf80b5783feb8b7fe0e5427ea5f
Aidan Noll <aidan.noll@gmail.com>
2013-11-30 18:05:52 -0500

    butterflies: i fucking hate these things

--- toontown/hood/TTHoodAI.py
+++ toontown/hood/TTHoodAI.py
@@ -4,6 +4,7 @@ from toontown.toon import NPCToons
 from toontown.dna.DNAParser import DNAStorage
 from toontown.town.TTStreetAI import TTStreetAI
 from HoodAI import HoodAI
+from toontown.safezone import ButterflyGlobals
 from toontown.safezone.DistributedButterflyAI import DistributedButterflyAI
 
 class TTHoodAI(HoodAI):
@@ -32,8 +33,10 @@ class TTHoodAI(HoodAI):
             self.streets[branch] = street
             
     def createButterflies(self):
-        for i in range(20):
-            butterfly = DistributedButterflyAI(self.air)
-            butterfly.setArea(0, 0)
-            butterfly.setState(0, 0, 0, 1, 1)
-            butterfly.generateWithRequired(self.HOOD)
+        playground = ButterflyGlobals.TTC
+        for area in range(ButterflyGlobals.NUM_BUTTERFLY_AREAS[playground]):
+            for b in range(ButterflyGlobals.NUM_BUTTERFLIES[playground]):
+                butterfly = DistributedButterflyAI(self.air)
+                butterfly.setArea(0, area)
+                butterfly.setState(0, 0, 0, 1, 1)
+                butterfly.generateWithRequired(self.HOOD)
--- toontown/safezone/DistributedButterflyAI.py
+++ toontown/safezone/DistributedButterflyAI.py
@@ -1,6 +1,8 @@
 from direct.directnotify import DirectNotifyGlobal
 from direct.distributed.DistributedObjectAI import DistributedObjectAI
+from direct.distributed.ClockDelta import *
 import ButterflyGlobals
+import random
 
 class DistributedButterflyAI(DistributedObjectAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedButterflyAI")
@@ -14,6 +16,21 @@ class DistributedButterflyAI(DistributedObjectAI):
         self.destIndex = 0
         self.time = 0
         self.timestamp = 0
+    
+    def generate(self):
+        fr = ButterflyGlobals.getFirstRoute(self.playground, self.area, self.doId)
+        self.b_setState(ButterflyGlobals.FLYING, fr[1], fr[3], fr[4], globalClockDelta.getRealNetworkTime())
+        taskMgr.doMethodLater(fr[4], self.__land, 'landButterfly%i' % self.doId, [])
+    
+    def __land(self):
+        ttl = random.uniform(0, ButterflyGlobals.MAX_LANDED_TIME)
+        self.b_setState(ButterflyGlobals.LANDED, self.curIndex, self.destIndex, ttl, globalClockDelta.getRealNetworkTime())
+        taskMgr.doMethodLater(ttl, self.__fly, 'flyButterfly%i' % self.doId, [])
+        
+    def __fly(self):
+        next = ButterflyGlobals.getNextPos(ButterflyGlobals.ButterflyPoints[self.playground][self.area][self.destIndex], self.playground, self.area, self.doId)
+        self.b_setState(ButterflyGlobals.FLYING, self.destIndex, next[1], next[2], globalClockDelta.getRealNetworkTime())
+        taskMgr.doMethodLater(next[2], self.__land, 'landButterfly%i' % self.doId, [])
     
     def setArea(self, playground, area):
         self.area = area
