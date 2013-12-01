24a339e96d4c6824bca27616640962a2945f68ba
Harvir <harvir14@gmail.com>
2013-12-01 16:42:45 +0000

    alpha: Remove all accessories and effects from non-staff members.

--- toontown/toon/DistributedToonAI.py
+++ toontown/toon/DistributedToonAI.py
@@ -4419,11 +4419,20 @@ class DistributedToonAI(DistributedPlayerAI.DistributedPlayerAI, DistributedSmoo
             emotes[emoteId] = 1
 
         self.b_setEmoteAccess(emotes)
-        #Remove Hat after Thanksgiving
-        self.b_setHat(0, 0, 0)        
+      
         #Toons with cheesy effects 16, 17 and 18 shouldn't stay persistant.
         if self.savedCheesyEffect == 16 or self.savedCheesyEffect == 17 or self.savedCheesyEffect == 18:
             self.b_setCheesyEffect(0, 0, 0)
+            
+        # Too many alpha testers complained. Remove all effects/accessories from non-GMs.
+        if not self.isGM():
+            self.b_setCheesyEffect(0, 0, 0)
+            self.b_setHat(0, 0, 0)
+            self.b_setGlasses(0, 0, 0)
+            self.b_setShoes(0, 0, 0)
+        # Joey doesn't want backpacks.
+        if self._gmType != 2:
+            self.b_setBackpacks(0, 0, 0)
 
 @magicWord(category=CATEGORY_CHARACTERSTATS, types=[int, int, int])
 def setCE(CEValue, CEHood=0, CEExpire=0):
