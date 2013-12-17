87c6f3970d726018a966273b7fa5c95324fa1b3e
Harvir <harvir14@gmail.com>
2013-12-17 17:46:22 +0000

    dna: Oopsie, spawn the Kart Shop in Goofy Speedway.

--- toontown/dna/DNASpawnerAI.py
+++ toontown/dna/DNASpawnerAI.py
@@ -31,7 +31,7 @@ class DNASpawnerAI:
         
     def spawnObjects(self, filename, baseZone):
         # This is strictly for buildings during alpha release
-        self.spawnInteriorsIn = [1000, 2000]
+        self.spawnInteriorsIn = [1000, 2000, 8000] # 8000 = GSW... and kart shop was already open ;D
         self.spawnNPCsIn = [2000]
         
         dnaStore = DNAStorage()
