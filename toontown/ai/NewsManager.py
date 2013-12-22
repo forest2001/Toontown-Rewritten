b746e2e2dd33946548666d694f71ef7b44f37a82
Joey Z <joey1998212@gmail.com>
2013-12-22 17:40:57 -0500

    Enable Winter menu in a cheaty way, remove "Seasons Greetings" because that is the worst of all holiday greetings

--- toontown/ai/NewsManager.py
+++ toontown/ai/NewsManager.py
@@ -50,6 +50,7 @@ class NewsManager(DistributedObject.DistributedObject):
         base.cr.newsManager = self
         base.localAvatar.inventory.setInvasionCreditMultiplier(1)
         self.weeklyCalendarHolidays = []
+        base.localAvatar.chatMgr.chatInputSpeedChat.addCarolMenu()
         return
 
     def delete(self):
