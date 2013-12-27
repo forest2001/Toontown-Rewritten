054716d480b56dd8ed9b7b6f33be74c2fe3fa648
Harvir <harvir14@gmail.com>
2013-12-27 20:19:10 +0000

    snowballs: Clean up snowball NPC code.

--- toontown/toon/DistributedNPCSnowballGiverAI.py
+++ toontown/toon/DistributedNPCSnowballGiverAI.py
@@ -9,19 +9,6 @@ class DistributedNPCSnowballGiverAI(DistributedNPCToonBaseAI):
     def __init__(self, air, npcId, questCallback = None, hq = 0):
         DistributedNPCToonBaseAI.__init__(self, air, npcId, questCallback)
         self.air = air
-        self.hq = hq
-        self.tutorial = 0
-        self.pendingAvId = None
-        return
-
-    def getTutorial(self):
-        return self.tutorial
-
-    def setTutorial(self, val):
-        self.tutorial = val
-
-    def getHq(self):
-        return self.hq
 
     def avatarEnter(self):
         avId = self.air.getAvatarIdFromSender()
@@ -36,14 +23,3 @@ class DistributedNPCSnowballGiverAI(DistributedNPCToonBaseAI):
         #self.air.questManager.requestInteract(avId, self)
         #DistributedNPCToonBaseAI.avatarEnter(self)
         #self.rejectAvatar(avId)
-
-    def setMovieDone(self):
-        avId = self.air.getAvatarIdFromSender()
-        self.notify.debug('setMovieDone busy: %s avId: %s' % (self.busy, avId))
-        if self.busy == avId:
-            taskMgr.remove(self.uniqueName('clearMovie'))
-            self.sendClearMovie(None)
-        elif self.busy:
-            self.air.writeServerEvent('suspicious', avId, 'DistributedNPCToonAI.setMovieDone busy with %s' % self.busy)
-            self.notify.warning('somebody called setMovieDone that I was not busy with! avId: %s' % avId)
-        return
