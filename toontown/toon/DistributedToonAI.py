a728e0db8d69e87dbab3850ea03ddb76bd3859d7
Harvir <harvir14@gmail.com>
2013-12-29 12:55:25 +0000

    magicwords: Don't allow an admin to give out wedding cakes.

--- toontown/toon/DistributedToonAI.py
+++ toontown/toon/DistributedToonAI.py
@@ -4865,13 +4865,15 @@ def setTrophyScore(value):
 @magicWord(category=CATEGORY_OVERRIDE, types=[int, int])
 def givePies(pieType, numPies=0):
     """Give target Y number of X pies."""
+    av = spellbook.getTarget()
     if pieType == -1:
         av.b_setNumPies(0)
         return "Removed %s's pies." % spellbook.getTarget().getName()
+    if pieType == 6:
+        return "Wedding cake 'pie' cause client crashes!"
     if not 0 <= pieType <= 7:
         return "pieType value out of range (0-7)"
     if not 0 <= numPies <= 99:
         return "numPies value out of range (0-99)"
-    av = spellbook.getTarget()
     av.b_setPieType(pieType)
     av.b_setNumPies(numPies)
