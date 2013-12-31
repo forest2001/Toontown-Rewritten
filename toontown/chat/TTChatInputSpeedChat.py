f2a7978bdc7a7c5a080cb26400fd41889dbaf8ca
Joey Z <joey1998212@gmail.com>
2013-12-31 15:58:41 -0500

    speedchat: Remove winter phrases

--- toontown/chat/TTChatInputSpeedChat.py
+++ toontown/chat/TTChatInputSpeedChat.py
@@ -17,18 +17,7 @@ from toontown.shtiker.OptionsPage import speedChatStyles
 from toontown.toonbase import TTLocalizer
 from toontown.parties.PartyGlobals import ActivityIds, DecorationIds
 from toontown.toonbase import ToontownGlobals
-scStructure = [[OTPLocalizer.SCMenuWinter,
-  [OTPLocalizer.SCMenuCarol,
-   30220,
-   30221,
-   30222,
-   30223,
-   30224,
-   30225],
-  30275,
-  30276,
-  30277],
- [OTPLocalizer.SCMenuTesting,
+scStructure = [[OTPLocalizer.SCMenuTesting,
   [OTPLocalizer.SCMenuBugs,
    5406,
    5407,
