49c5373d76840696f44c9c235b5aa1c697aa2af5
Harvir <harvir14@gmail.com>
2014-01-18 19:24:44 +0000

    estates/friends: Fix my rushed derpness.

--- toontown/friends/TTRFriendsManager.py
+++ toontown/friends/TTRFriendsManager.py
@@ -61,7 +61,7 @@ class TTRFriendsManager(DistributedObjectGlobal):
             
             # TEMPORARY, UNTIL TPING TO FRIENDS ESTATES WORKS
             self.sendUpdate('routeTeleportResponse', [ fromId, 0, 0, 0, 0 ])
-            base.localAvatar.setSystemMesssage(0,'%s tried to visit you, but Portable Holes can\'t get here yet!' % base.cr.identifyFriend(fromId).getName())
+            base.localAvatar.setSystemMessage(0, '%s tried to visit you, but Portable Holes can\'t get here yet!' % base.cr.identifyFriend(fromId).getName())
             return
             # TEMPORARY, UNTIL TPING TO FRIENDS ESTATES WORKS
         else:
