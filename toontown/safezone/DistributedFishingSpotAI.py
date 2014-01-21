fa9525d71c37b1a2796f1881bbc48bf572d8bb5c
Aidan <aidan.noll@gmail.com>
2014-01-21 18:23:13 -0500

    fishing: revert stupid change

--- toontown/safezone/DistributedFishingSpotAI.py
+++ toontown/safezone/DistributedFishingSpotAI.py
@@ -103,8 +103,8 @@ class DistributedFishingSpotAI(DistributedObjectAI):
         if self.avId != avId:
             self.air.writeServerEvent('suspicious', avId, 'Toon tried to sell fish at a pier they\'re not using!')
             return
-        #if self.air.doId2do[pondDoId].getArea() != ToontownGlobals.MyEstate:
-        #    self.air.writeServerEvent('suspicious', avId, 'Toon tried to sell fish at a pier not in their estate!')
+        if self.air.doId2do[pondDoId].getArea() != ToontownGlobals.MyEstate:
+            self.air.writeServerEvent('suspicious', avId, 'Toon tried to sell fish at a pier not in their estate!')
         av = self.air.doId2do[avId]
         result = self.air.fishManager.creditFishTank(av)
         totalFish = len(av.fishCollection)
