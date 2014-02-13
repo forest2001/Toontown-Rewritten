0491a651e98d81a7c90daf03840ad18b260213a1
Sam Edwards <CFSworks@gmail.com>
2014-02-13 16:34:45 -0700

    election: Give invading Cogs a proxemics system to prevent them from marching around inside each other.

--- toontown/election/InvasionSuitBrainAI.py
+++ toontown/election/InvasionSuitBrainAI.py
@@ -109,14 +109,86 @@ class HarassBehavior(AttackBehavior):
     def exitAttackCooldown(self):
         self.__wait.remove()
 
+# We got too close to another Cog and have to spread out a little...
+class UnclumpBehavior(FSM):
+    UNCLUMP_SCAN_RADIUS = 30
+    UNCLUMP_MOVE_DISTANCE = 5
+    UNCLUMP_WAIT_TIME = 1.0
+
+    def __init__(self, brain):
+        FSM.__init__(self, 'UnclumpFSM')
+
+        self.brain = brain
+
+    def start(self):
+        moveVector = Vec2()
+
+        # Scan for who else is where...
+        ourSuit = self.brain.suit
+        ourPos = ourSuit.getCurrentPos()
+        for otherSuit in self.brain.suit.invasion.suits:
+            if otherSuit == ourSuit:
+                continue # It's us!
+
+            otherPos = otherSuit.getCurrentPos()
+
+            moveAway = ourPos - otherPos
+
+            if moveAway.length() > self.UNCLUMP_SCAN_RADIUS:
+                continue # Too far away to consider them.
+
+            # Use an inverse square law to scale the "move away" vector.
+            moveMag = 1.0/moveAway.lengthSquared()
+            moveAway.normalize()
+            moveAway *= moveMag
+
+            # Add it to to our overall move direction.
+            moveVector += moveAway
+
+        # Which direction do we go?
+        moveVector.normalize()
+        x, y = ourPos + (moveVector * self.UNCLUMP_MOVE_DISTANCE)
+
+        if self.brain.navigateTo(x, y):
+            # And we're walking!
+            self.demand('Walking')
+        else:
+            # Hmm... Can't walk there. Let's just idle for a bit instead.
+            self.demand('Wait')
+
+    def enterWalking(self):
+        pass # Do nothing, we just wait for onArrive and exit the behavior.
+
+    def onArrive(self):
+        if self.state == 'Walking':
+            # Ah, here we are!
+            self.brain.demand('Idle')
+
+    def enterWait(self):
+        self.brain.suit.idle()
+        self._waitDelay = \
+            taskMgr.doMethodLater(self.UNCLUMP_WAIT_TIME, self._doneWaiting,
+                                  self.brain.suit.uniqueName('unclump-wait'))
+
+    def _doneWaiting(self, task):
+        self.brain.demand('Idle')
+        return task.done
+
+    def exitWait(self):
+        self._waitDelay.remove()
 
 class InvasionSuitBrainAI(FSM):
+    PROXEMICS_INTERVAL = 0.5
+    PERSONAL_SPACE = 5
+
     def __init__(self, suit):
         FSM.__init__(self, 'InvasionSuitBrainFSM')
         self.suit = suit
         self.master = self.suit.invasion.master
 
         self.behavior = None
+
+        self.__proxemicsTask = None
 
         # For the nav system:
         self.__waypoints = []
@@ -134,6 +206,62 @@ class InvasionSuitBrainAI(FSM):
 
     def getAttackRange(self):
         return 20.0
+
+    def enterOff(self):
+        self.__stopProxemics()
+
+    def exitOff(self):
+        self.__startProxemics()
+
+    def enterUnclump(self):
+        self.__stopProxemics()
+
+        self.behavior = UnclumpBehavior(self)
+        self.behavior.start()
+
+    def exitUnclump(self):
+        self.__startProxemics()
+
+        self.behavior.demand('Off')
+        self.behavior = None
+
+    def __startProxemics(self):
+        if not self.__proxemicsTask:
+            self.__proxemicsTask = \
+                taskMgr.doMethodLater(self.PROXEMICS_INTERVAL, self.__proxemics,
+                                      self.suit.uniqueName('proxemics'))
+
+    def __stopProxemics(self):
+        if self.__proxemicsTask:
+            self.__proxemicsTask.remove()
+            self.__proxemicsTask = None
+
+    def __proxemics(self, task):
+        # Proxemics: Maintain personal space. This works by yielding to the
+        # higher-leveled (or, in the event of the same level, lower-ID'd) Cog.
+
+        for otherSuit in self.suit.invasion.suits:
+            if otherSuit == self.suit:
+                continue # It's us!
+
+            if otherSuit.getActualLevel() < self.suit.getActualLevel():
+                # We outrank them, and do not yield to subordinates!
+                continue
+            elif (otherSuit.getActualLevel() == self.suit.getActualLevel() and
+                  otherSuit.doId > self.suit.doId):
+                # We may not outrank them, but our ID is lower - we were first!
+                continue
+
+            # Okay, we have to consider yielding to them. Are we too close?
+            ourPos = self.suit.getCurrentPos()
+            otherPos = otherSuit.getCurrentPos()
+
+            if (ourPos - otherPos).length() < self.PERSONAL_SPACE:
+                # Yeah, too close...
+                self.demand('Unclump')
+                return task.done
+
+        return task.again
 
     def enterIdle(self):
         # Brain has nothing to do -- ask the master for orders:
