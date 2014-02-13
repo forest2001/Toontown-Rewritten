617c3f9968c52a6a5efaed98073cbf4fa3d56425
Sam Edwards <CFSworks@gmail.com>
2014-02-12 21:19:54 -0700

    election: Fully map out the obstacles in TTC for pathfinding.

--- toontown/election/InvasionPathDataAI.py
+++ toontown/election/InvasionPathDataAI.py
@@ -1,4 +1,26 @@
+import math
 from InvasionPathfinderAI import InvasionPathfinderAI
+
+# Some helper functions for below:
+def rect(x1, y1, x2, y2):
+    minX = min(x1, x2)
+    maxX = max(x1, x2)
+    minY = min(y1, y2)
+    maxY = max(y1, y2)
+
+    return [(minX, minY), (minX, maxY), (maxX, maxY), (maxX, minY)]
+
+def circle(x, y, radius, vertices=5):
+    # Doesn't actually define a "circle" -- more of a pentagon. :)
+    result = []
+    for theta in range(0, 360, 360/vertices):
+        theta *= math.pi/180 # Degrees to radians
+        result.append((x + math.sin(theta)*radius, y + math.cos(theta)*radius))
+    return result
+
+LampPostRadius = 2
+SmallTreeRadius = 2
+FatTreeRadius = 4
 
 # All of the polygons that define where Cogs can and can't walk.
 # NOTE: All of the polygons must have their vertices defined in clockwise order,
@@ -65,12 +87,148 @@ InvasionPathPolygons = [
         (-145.703,  -25.070),
     ],
 
-    # Mickey statue:
+    # ------Playground structure------
+    # East hedges + vase plateau
     [
-        (82.227,  128.010),
-        (82.103,  114.525),
-        (67.323,  113.691),
-        (65.675,  127.920),
+        (8.959,  -52.802),
+        (21.272,  -36.824),
+        (31.399,  -49.620),
+        (22.512,  -61.303),
+        (30.619,  -70.424),
+        (49.778,  -71.356),
+        (49.492,  -77.399),
+        (27.020,  -77.335),
+        (16.039,  -64.547),
+    ],
+    # West hedges, vase plateau + Toon HQ:
+    [
+        (9.888,  50.510),
+        (16.599,  64.437),
+        (27.340,  77.828),
+        (64.581,  77.371),
+        (65.007,  71.412),
+        (29.839,  71.132),
+        (21.573,  60.877),
+        (47.202,  30.517),
+        (31.546,  14.884),
+        (15.470,  31.820),
+        (19.444,  38.602),
+    ],
+    # Gazebo, west wall:
+    [
+        (-67.242,  -7.562),
+        (-75.214,  -4.731),
+        (-74.251,  -1.214),
+        (-69.356,  1.903),
+        (-48.325,  -0.739),
+        (-46.772,  -4.773),
+        (-55.003,  -7.259),
+    ],
+    # Gazebo, east wall:
+    [
+        (-55.031,  -10.064),
+        (-46.126,  -11.580),
+        (-48.464,  -17.958),
+        (-73.640,  -18.398),
+        (-76.184,  -12.535),
+        (-67.085,  -10.405),
+    ],
+    # Lake, west of bridge, plus west bridge arch:
+    [
+        (-101.7, -4.5),
+        (-101.7, -0.4),
+        (-97.206,  19.945),
+        (-99.302,  31.242),
+        (-93.146,  44.609),
+        (-85.646,  50.163),
+        (-60.777,  48.316),
+        (-50.997,  26.928),
+        (-49.979,  17.245),
+        (-57.210,  13.057),
+        (-72.451,  15.295),
+        (-82.538,  5.710),
+        (-80.0, -0.4),
+        (-80.0, -4.5)
+    ],
+    # Lake, east of bridge, plus east bridge arch:
+    [
+        (-80.0, -11.2),
+        (-81.416,  -23.633),
+        (-69.558,  -30.282),
+        (-54.587,  -27.265),
+        (-57.747,  -37.523),
+        (-74.652,  -46.688),
+        (-84.467,  -47.163),
+        (-95.107,  -30.514),
+        (-101.7, -11.2),
+        (-101.7, -7.1),
+        (-80.0, -7.1),
+    ],
+
+    # Mickey statue:
+    rect(82.2, 128.0, 65.6, 113.6),
+
+    # Goofy fountain:
+    circle(93.2, -106.4, 16, 8),
+
+    # Lamp posts:
+    circle(3.8, 118.5, LampPostRadius),
+    circle(117.0, 147.0, LampPostRadius),
+    circle(86.8, 164.8, LampPostRadius),
+    circle(43.7, -86.2, LampPostRadius),
+    circle(77.3, -86.4, LampPostRadius),
+    circle(58.9, 93.7, LampPostRadius),
+    circle(92.8, 93.7, LampPostRadius),
+    circle(132.8, -122.5, LampPostRadius),
+    circle(5.0, -116.2, LampPostRadius),
+    circle(109.0, -28.1, LampPostRadius),
+    circle(108.2, 32.1, LampPostRadius),
+    circle(33.0, 61.9, LampPostRadius),
+    circle(28.9, -57.0, LampPostRadius),
+    circle(-102.0, -70.5, LampPostRadius),
+    circle(-129.9, -39.6, LampPostRadius),
+    circle(-125, 60, LampPostRadius),
+
+    # Trees:
+    circle(-79.8, 79.5, SmallTreeRadius),
+    circle(-127.0, 30.4, SmallTreeRadius),
+    circle(-128.2, -24.0, SmallTreeRadius),
+    circle(119.6, -127.9, SmallTreeRadius),
+    circle(127.4, -59.1, SmallTreeRadius),
+    circle(120.1, -44.5, SmallTreeRadius),
+    circle(96.9, -146.4, SmallTreeRadius),
+    circle(114.1, -57.3, SmallTreeRadius),
+    circle(142.7, 108.7, FatTreeRadius),
+    circle(6.3, 100.9, FatTreeRadius),
+    circle(-24.0, 74.2, SmallTreeRadius),
+    circle(103.4, 79.4, FatTreeRadius),
+    circle(116.1, 54.8, FatTreeRadius),
+    circle(6.5, -96.7, FatTreeRadius),
+    circle(-53.7, -73.3, SmallTreeRadius),
+    circle(55.4, 154.7, FatTreeRadius),
+
+    # ------ Buildings ------
+    # Library:
+    [
+        (43.596,  -19.401),
+        (78.205,  -19.732),
+        (86.742,  -32.561),
+        (87.124,  -55.755),
+        (78.209,  -65.005),
+        (43.309,  -65.455),
+        (34.761,  -56.078),
+        (34.408,  -32.424),
+    ],
+    # Bank:
+    [
+        (55.057,  59.839),
+        (58.309,  68.780),
+        (93.410,  68.583),
+        (97.441,  59.121),
+        (97.421,  34.923),
+        (93.758,  20.878),
+        (58.610,  20.347),
+        (54.879,  33.076),
     ],
 ]
 
