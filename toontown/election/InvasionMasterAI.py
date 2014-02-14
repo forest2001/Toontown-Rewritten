cb0c50b3d49d1f0373774a3912c32fe79ed4c805
Sam Edwards <CFSworks@gmail.com>
2014-02-13 17:27:28 -0700

    election: Update InvasionMasterAI to attack Toons at random.

--- toontown/election/InvasionMasterAI.py
+++ toontown/election/InvasionMasterAI.py
@@ -1,7 +1,18 @@
+import random
+
 class InvasionMasterAI:
     def __init__(self, invasion):
         self.invasion = invasion
 
     def requestOrders(self, brain):
-        # A Suit brain is requesting orders. For now, we just leave them idle.
+        # A Suit brain is requesting orders. For now, we just attack a Toon at
+        # random...
+        if not self.invasion.toons:
+            pass # No Toons in area, let Suits idle for the rest of time.
+
+        toonId = random.choice(self.invasion.toons).doId
+        brain.demand('Attack', toonId)
+
+    def toonUnreachable(self, toonId):
+        # A Cog couldn't get to a Toon to attack them.
         pass
