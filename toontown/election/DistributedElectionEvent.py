922fdcbc64fb4d50726af60bf82ad0dffe994aa4
Joey Z <joey1998212@gmail.com>
2014-02-21 23:25:48 -0500

    election: Add ElectionGlobals

--- toontown/election/DistributedElectionEvent.py
+++ toontown/election/DistributedElectionEvent.py
@@ -8,21 +8,7 @@ from toontown.toon import NPCToons
 from toontown.suit import Suit, SuitDNA
 from direct.task import Task
 from toontown.toonbase import ToontownGlobals
-
-# TODO: ElectionGlobals (especially since it's scripted!)
-FLIPPY_WHEELBARROW_PIES = [
-    # Format: posHprScale
-    [1.16, 11.24, 7.00, 246.80, 351.25, 0.00, 1.60, 1.40, 1.8],
-    [2.27, 8.02, 6.35, 104.04, 311.99, 9.46, 1.35, 1.35, 1],
-    [-1.23, 7.33, 6.88, 276.34, 28.61, 350.54, 1.41, 1.41, 1.6],
-    [0.27, 8.24, 6.42, 198.15, 351.87, 355.24, 1.93, 2, 2],
-    [0.06, 5.23, 6.78, 63.43, 355.91, 15.26, 1.3, 1.6, 1.8],
-    [-0.81, 11.37, 6.82, 326.31, 5.19, 19.98, 1.76, 1.86, 1.5],
-    [1.35, 10.09, 5.92, 35.54, 353.66, 343.30, 1.50, 1.90, 1.8],
-    [1.9, 5.59, 6.5, 75.96, 326.31, 8, 1.76, 1.56, 1.5],
-    [-1.74, 5.42, 6.28, 327.53, 318.81, 4.76, 1.8, 2, 2],
-    [-1.55, 9.22, 5.72, 266.53, 341.57, 0.00, 2.09, 1.68, 1.81],
-]
+import ElectionGlobals
 
 class DistributedElectionEvent(DistributedObject, FSM):
     def __init__(self, cr):
@@ -30,7 +16,7 @@ class DistributedElectionEvent(DistributedObject, FSM):
         FSM.__init__(self, 'ElectionFSM')
         self.cr.election = self
 
-        self.showFloor = NodePath('ShowFloor') #This currently isn't part of the main geometry, so it isn't dimmed with the sky. Will fix later.
+        self.showFloor = NodePath('ShowFloor')
         self.showFloor.setPos(80, 0, 4)
 
         #Stage
@@ -76,7 +62,7 @@ class DistributedElectionEvent(DistributedObject, FSM):
         pie.setPosHprScale(-2.8, -2.4, 6.1, 0, 355.24, 351.87, 2, 2.1, 1.6)
         pieS.setPosHprScale(3.54, -3.94, 0.42, 45.00, 42.27, 0, 1.6, 1.6, 1.6)
         # Pies in the wheelbarrow.
-        for pieSettings in FLIPPY_WHEELBARROW_PIES:
+        for pieSettings in ElectionGlobals.FLIPPY_WHEELBARROW_PIES:
             pieModel = pie.copyTo(wheelbarrow)
             pieModel.setPosHprScale(*pieSettings)
         self.restockSfx = loader.loadSfx('phase_9/audio/sfx/CHQ_SOS_pies_restock.ogg')
