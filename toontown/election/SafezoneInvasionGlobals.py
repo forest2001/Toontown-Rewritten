89e8202fcc3c32a9b76994486c54016e0d34880e
Joey Z <joey1998212@gmail.com>
2014-02-23 03:19:49 -0500

    doomsday: Add two new wave types - Wait Waves and Intermission Waves
    By default, waves now spawn immidiately after the previous so we have a constant stream of cogs during the battle rather than all at once. "Wait Waves" will wait until all cogs are destroyed before starting the next one, and "Intermission Waves" will act as waves did before and spawn a new wave after 20 seconds.

--- toontown/election/SafezoneInvasionGlobals.py
+++ toontown/election/SafezoneInvasionGlobals.py
@@ -13,13 +13,24 @@ SuitSpawnPoints = [
 ]
 
 SuitWaves = [
-    # WAVE 0:
-    ['f', 'bf', 'sc', 'cc'],
+    # Suits in a wave can't exceed spawn points.
+    # While each index is actually separate wave, they will keep
+    # spawning until the intermission wave, which is defined below.
+
     # WAVE 1:
-    ['f', 'f', 'f', 'bf', 'bf', 'sc', 'sc', 'cc'],
+    ['f', 'bf'],
+    ['sc', 'cc', 'sc'], #Wait Wave
+    ['f', 'bf', 'sc', 'cc', 'f', 'bf', 'sc', 'cc', 'sc'],
+
     # WAVE 2:
-    ['pp', 'ym', 'pp'],
+    ['pp', 'ym', 'pp']
 ]
+
+# On these waves, no more waves will spawn until all suits are destroyed.
+# Once the suits are destroyed, the next wave will spawn again instantly with no intermission.
+SuitWaitWaves = [1, 4, 7, 10]
+# These waves have a 20 second intermission period after all suits are destroyed.
+SuitIntermissionWaves = [2, 5, 8, 11]
 
 # This should be at least 6.5 (the suit fly-down time)
 WaveBeginningTime = 10
@@ -30,5 +41,6 @@ IntermissionTime = 20
 # How much healing does a pie on a Toon do?
 ToonHealAmount = 1
 
+# Let's define some needed files
 CogSkyFile = 'phase_3.5/models/props/BR_sky'
-InvasionMusicEnter = 'phase_4/audio/bgm/DD_main_temp.ogg' #This is temporary until we have all pieces of the song in separate files.
+InvasionMusicEnter = 'phase_4/audio/bgm/DD_main_temp.ogg' # TODO: Break into separate parts for a better loop
