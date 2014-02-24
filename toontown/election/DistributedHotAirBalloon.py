79e62c934a82bb5c93eadacec4ce37cb9aa1c057
Anthony Castelli <neueanthony@me.com>
2014-02-24 11:10:50 -0800

    Uncapitalized the ElectionGlobal names

--- toontown/election/DistributedHotAirBalloon.py
+++ toontown/election/DistributedHotAirBalloon.py
@@ -19,8 +19,8 @@ class DistributedHotAirBalloon(DistributedObject, FSM):
         # Create the balloon
         self.balloon = loader.loadModel('phase_4/models/events/airballoon.egg') # TODO: Use .bam model
         self.balloon.reparentTo(base.render)
-        self.balloon.setPos(*ElectionGlobals.BALLOON_BASE_POS)
-        self.balloon.setScale(ElectionGlobals.BALLOON_SCALE)
+        self.balloon.setPos(*ElectionGlobals.BalloonBasePosition)
+        self.balloon.setScale(ElectionGlobals.BalloonScale)
         # So we can reparent toons to the balloon so they don't fall out
         self.cr.parentMgr.registerParent(ToontownGlobals.SPSlappysBalloon, self.balloon)
         # Balloon collision NodePath (outside)
@@ -85,7 +85,7 @@ class DistributedHotAirBalloon(DistributedObject, FSM):
             self.hopOnAnim.start()
 
         self.occupiedSequence = Sequence(
-            Func(self.slappy.setChatAbsolute, choice(ElectionGlobals.SLAPPY_SPEECHES), CFSpeech | CFTimeout),
+            Func(self.slappy.setChatAbsolute, choice(ElectionGlobals.SlappySpeeches), CFSpeech | CFTimeout),
             Wait(3.5),
         )
         self.occupiedSequence.start()
@@ -97,7 +97,7 @@ class DistributedHotAirBalloon(DistributedObject, FSM):
     def enterStartRide(self, offset):
         # TODO: Choose a random route to fly on (or at least improve this one)
         self.rideSequence = Sequence(
-            Func(self.slappy.setChatAbsolute, ElectionGlobals.SLAPPY_RIDE_START, CFSpeech | CFTimeout),
+            Func(self.slappy.setChatAbsolute, ElectionGlobals.SlappyRideStartMessage, CFSpeech | CFTimeout),
 
             # Lift Off
             Wait(0.5),
@@ -106,26 +106,26 @@ class DistributedHotAirBalloon(DistributedObject, FSM):
 
             # To the tunnel we go
             Wait(0.5),
-            Func(self.slappy.setChatAbsolute, ElectionGlobals.SLAPPY_VIEW, CFSpeech | CFTimeout),
+            Func(self.slappy.setChatAbsolute, ElectionGlobals.SlappyViewMessage, CFSpeech | CFTimeout),
             self.balloon.posInterval(5.0, Point3(-125, 33, 54)),
             # 11 Seconds
 
             # Lets drop a weight on the gag shop
             Wait(0.5),
             self.balloon.posInterval(4.0, Point3(-100, -60, 54)),
-            Func(self.slappy.setChatAbsolute, ElectionGlobals.SLAPPY_WEIGHT_MISSED, CFSpeech | CFTimeout),        
+            Func(self.slappy.setChatAbsolute, ElectionGlobals.SlappyWeightMissedMessage, CFSpeech | CFTimeout),        
             # 16.5 Seconds
 
             # Rats, we missed! Lets checkout the podium
             Wait(0.5),
             self.balloon.posInterval(7.0, Point3(60, -10, 54)),
-            Func(self.slappy.setChatAbsolute, ElectionGlobals.SLAPPY_PODIUM, CFSpeech | CFTimeout),
+            Func(self.slappy.setChatAbsolute, ElectionGlobals.SlappyPodiumMessage, CFSpeech | CFTimeout),
             # 22 Seconds
 
             # Back to the Launchpad
             Wait(0.5),
             self.balloon.posInterval(4.0, Point3(-15, 33, 54)),
-            Func(self.slappy.setChatAbsolute, ElectionGlobals.SLAPPY_RIDE_DONE, CFSpeech | CFTimeout),
+            Func(self.slappy.setChatAbsolute, ElectionGlobals.SlappyRideDoneMessage, CFSpeech | CFTimeout),
             # 27.5 Seconds
 
             # Set her down; gently
