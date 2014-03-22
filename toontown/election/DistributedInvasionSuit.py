9b78328fad32d885a72c98bcc2d27a9cad01d642
Aidan Noll <aidan.noll@gmail.com>
2014-03-22 11:15:04 -0400

    doomsday: fix position

--- toontown/election/DistributedInvasionSuit.py
+++ toontown/election/DistributedInvasionSuit.py
@@ -235,18 +235,23 @@ class DistributedInvasionSuit(DistributedSuitBase, InvasionSuitBase, FSM):
         self.prop = BattleProps.globalPropPool.getProp(self.attackProp)
         self.propIsActor = True
         animName = 'throw-paper'
+        x, y, z, h, p, r = (0.1, 0.2, -0.35, 0, 336, 0)
         if self.attackProp == 'redtape':
             animName = 'throw-object'
+            x,y,z,h,p,r = (0.24, 0.09, -0.38, -1.152, 86.581, -76.784)
             self.propIsActor = False
         elif self.attackProp == 'newspaper':
             animName = 'throw-object'
             self.propIsActor = False
+            x,y,z,h,p,r = (-0.07, 0.17, -0.13, 161.867, -33.149, -48.086)
             self.prop.setScale(4)
         elif self.attackProp == 'pink-slip':
             self.propIsActor = False
+            x,y,z,h,p,r = (0.07, -0.06, -0.18, -172.075, -26.715, -89.131)
             self.prop.setScale(5)
         elif self.attackProp == 'power-tie':
             self.propIsActor = False
+            x,y,z,h,p,r = (1.16, 0.24, 0.63, 171.561, 1.745, -163.443)
             self.prop.setScale(4)
 
         # Prop collisions:
@@ -292,7 +297,7 @@ class DistributedInvasionSuit(DistributedSuitBase, InvasionSuitBase, FSM):
             ActorInterval(self, animName),
             Track(
                 (0.4, Func(self.prop.reparentTo, self.getRightHand())),
-                (0.0, Func(self.prop.setPosHpr, 0.1, 0.2, -0.35, 0, 336, 0)),
+                (0.0, Func(self.prop.setPosHpr, x, y, z, h, p, r)),
                 (0.0, Func(self.sayFaceoffTaunt)),
                 (throwDelay, Func(throwProp)),
                 (10.0, Func(self.cleanupProp))
