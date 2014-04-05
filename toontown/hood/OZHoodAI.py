31518abaabf6ae141c2a7676e8d75e06f869009b
Joey Z <joey1998212@gmail.com>
2014-04-05 16:29:52 -0400

    hood: Clean up OZHoodAI from Harv's suggestions

--- toontown/hood/OZHoodAI.py
+++ toontown/hood/OZHoodAI.py
@@ -1,11 +1,11 @@
 from HoodAI import HoodAI
 from toontown.toonbase import ToontownGlobals
-from toontown.distributed import DistributedTimerAI
+from toontown.distributed.DistributedTimerAI import DistributedTimerAI
 
 class OZHoodAI(HoodAI):
     HOOD = ToontownGlobals.OutdoorZone
     
     def createSafeZone(self):
         HoodAI.createTreasurePlanner(self)
-        self.timer = DistributedTimerAI.DistributedTimerAI(self.air)
-        self.timer.generateWithRequired(ToontownGlobals.OutdoorZone)
+        self.timer = DistributedTimerAI(self.air)
+        self.timer.generateWithRequired(self.HOOD)
