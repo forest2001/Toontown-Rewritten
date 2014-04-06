dna: Re-add sign typesetting. Baseline parameters are not in the correct scaling space yet.

toontown/dna/DNABaseline.py
@@ -6,19 +6,27 @@ class DNABaseline(DNASceneElement):
     TAG = 'baseline'
     PARENTS = ['sign']
 
-    def __init__(self, code=None, flags='', wiggle='0', stumble='0', stomp='0', kern='0', width='0', height='0'):
+    def __init__(self, code=None, flags='', wiggle='0', stumble='0', indent='0', stomp='0', kern='0', width='0', height='0'):
         DNASceneElement.__init__(self)
 
         self.code = code
         self.flags = flags
+
+        # TODO: Text spacing parameters must have the scaling factor applied.
         self.wiggle = float(wiggle)
         self.stumble = float(stumble)
+        self.indent = float(indent)
         self.stomp = float(stomp)
         self.kern = float(kern)
         self.width = float(width)
         self.height = float(height)
 
     def _makeNode(self, storage, parent):
-        pass # TODO
+        np = parent.attachNewNode('baseline')
+
+        # Don't z-fight with the sign.
+        np.setDepthOffset(50)
+
+        return np
 
 registerElement(DNABaseline)

toontown/dna/DNAText.py
@@ -1,4 +1,5 @@
 from DNASceneElement import DNASceneElement
+from DNATypesetter import DNATypesetter
 from DNAParser import *
 from panda3d.core import *
 
@@ -14,8 +15,12 @@ class DNAText(DNASceneElement):
     def handleText(self, text):
         self.text = text
 
-
     def _makeNode(self, storage, parent):
-        pass # TODO
+        typesetter = DNATypesetter(self._parent, storage)
+
+        np = typesetter.generate(self.text)
+        if np:
+            np.reparentTo(parent)
+            return np
 
 registerElement(DNAText)