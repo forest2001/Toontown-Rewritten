174b61cb62dc2402049542c30a2a628dabf3f0fa
Sam Edwards <CFSworks@gmail.com>
2014-04-06 05:32:22 -0600

    dna: Make DNATexture texture its parent street node.

--- toontown/dna/DNATexture.py
+++ toontown/dna/DNATexture.py
@@ -9,16 +9,22 @@ class DNATexture(DNASceneElement):
     def __init__(self):
         DNASceneElement.__init__(self)
 
-        self.code = None
+        self.code = ''
 
     def handleText(self, text):
-        self.code = text
+        self.code += text
 
     def _makeNode(self, storage, parent):
         texture = storage.findTexture(self.code)
         if texture is None:
             raise DNAError('DNATexture uses unknown code %s' % self.code)
 
-        pass # TODO
+        index = parent.getPythonTag('texture_index') or 0
+
+        node = parent.find('**/*_' + ['street', 'sidewalk', 'curb'][index])
+        parent.setPythonTag('texture_index', index+1)
+
+        if not node.isEmpty():
+            node.setTexture(texture, 1)
 
 registerElement(DNATexture)
