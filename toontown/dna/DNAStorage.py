c9b615dbe5b8d5ff9da865d63b2179e4885e1f00
Sam Edwards <CFSworks@gmail.com>
2014-04-06 04:16:53 -0600

    dna: Create DNAStorage.

---
+++ toontown/dna/DNAStorage.py
@@ -0,0 +1,46 @@
+from panda3d.core import *
+
+class DNAStorageEntry:
+    def __init__(self, entity, type, code, scope):
+        self.type = type
+        self.code = code
+        self.scope = scope
+        self.entity = entity
+
+class DNAStorage:
+    def __init__(self):
+        self._typecode2entry = {}
+
+    def store(self, entity, type, code, scope):
+        entry = DNAStorageEntry(entity, type, code, scope)
+        self._typecode2entry[(type, code)] = entry
+
+    def find(self, type, code):
+        entry = self._typecode2entry.get((type, code))
+        if entry:
+            return entry.entity
+
+    def reset(self, type=None, scope=None):
+        toPurge = set()
+        for entry in self._typecode2entry.itervalues():
+            if type is not None and entry.type != type:
+                continue
+            if scope is not None and entry.scope != scope:
+                continue
+            toPurge.add(entry)
+
+        for purge in toPurge:
+            del self._typecode2entity[(purge.type, purge.code)]
+
+    # Helpers for the above:
+    def storeNode(self, node, code, scope):
+        self.store(node, 'node', code, scope)
+
+    def storeFont(self, font, code, scope):
+        self.store(font, 'font', code, scope)
+
+    def storeTexture(self, texture, code, scope):
+        self.store(texture, 'texture', code, scope)
+
+    def findNode(self, code):
+        return self.find('node', code)
