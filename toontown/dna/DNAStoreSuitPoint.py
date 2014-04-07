85942a29bcd0995edbfdd3af45284719f3073bf1
Maverick Mosher <MMavipc@hostedyou.com>
2014-04-07 01:17:13 -0700

    dna: added getIndex and getType to DNAStoreSuitPoint, also consts for suit point types

--- toontown/dna/DNAStoreSuitPoint.py
+++ toontown/dna/DNAStoreSuitPoint.py
@@ -2,6 +2,13 @@ from DNASceneElement import DNASceneElement
 from DNAParser import *
 from panda3d.core import *
 
+
+STREETPOINT = 0
+FRONTDOORPOINT = 1
+SIDEDOORPOINT = 2
+COGHQINPOINT = 3
+COGHQOUTPOINT = 4
+
 class DNAStoreSuitPoint(DNASceneElement):
     TAG = 'store_suit_point'
     PARENTS = ['scene']
@@ -15,6 +22,12 @@ class DNAStoreSuitPoint(DNASceneElement):
         self.y = float(y)
         self.z = float(z)
         self.building = building and int(building)
+
+    def getPointType(self):
+        return self.type
+
+    def getIndex(self):
+        return self.id
 
     def _storeData(self, data):
         data.suitPoints.append(self)
