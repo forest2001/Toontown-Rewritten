b96262378f45b0d73329811809f812acdb2cc942
Sam Edwards <CFSworks@gmail.com>
2014-04-07 19:07:43 -0600

    dna: Spawn knock-knock doors on the AI.

--- toontown/building/DistributedKnockKnockDoorAI.py
+++ toontown/building/DistributedKnockKnockDoorAI.py
@@ -1,6 +1,28 @@
 from direct.directnotify import DirectNotifyGlobal
 from toontown.building.DistributedAnimatedPropAI import DistributedAnimatedPropAI
+from toontown.dna.DNASpawnerAI import *
+from toontown.dna.DNAFlatDoor import DNAFlatDoor
+import re
 
 class DistributedKnockKnockDoorAI(DistributedAnimatedPropAI):
     notify = DirectNotifyGlobal.directNotify.newCategory("DistributedKnockKnockDoorAI")
 
+# DNA spawn code:
+buildingPattern = re.compile('tb([0-9]+):')
+@dnaSpawn(DNAFlatDoor)
+def spawn(air, zone, element):
+    # Get door's parent building:
+    building = element.parent.parent
+
+    # Parse building name to figure out index number:
+    match = buildingPattern.match(building.name)
+
+    if not match:
+        # No match! Can't spawn a door...
+        return
+
+    index = int(match.group(1))
+
+    door = DistributedKnockKnockDoorAI(air)
+    door.setPropId(index)
+    door.generateWithRequired(zone)
