19a0d2dd915b45f9fcc7e9dd247e69b687276994
Sam Edwards <CFSworks@gmail.com>
2014-04-08 02:56:43 -0600

    dna: Move texture application to DNAStreet, for performance (and to save a tag).

--- toontown/dna/DNATexture.py
+++ toontown/dna/DNATexture.py
@@ -14,17 +14,4 @@ class DNATexture(DNASceneElement):
     def handleText(self, text):
         self.code += text
 
-    def _makeNode(self, storage, parent):
-        texture = storage.findTexture(self.code)
-        if texture is None:
-            raise DNAError('DNATexture uses unknown code %s' % self.code)
-
-        index = parent.getPythonTag('texture_index') or 0
-
-        node = parent.find('**/*_' + ['street', 'sidewalk', 'curb'][index])
-        parent.setPythonTag('texture_index', index+1)
-
-        if not node.isEmpty():
-            node.setTexture(texture, 1)
-
 registerElement(DNATexture)
