d21ff55fb234d15e0f8e1a101bb07bd600e8d828
Sam Edwards <CFSworks@gmail.com>
2014-04-07 19:07:25 -0600

    dna: Implement DNAFlatDoor.

--- toontown/dna/DNAFlatDoor.py
+++ toontown/dna/DNAFlatDoor.py
@@ -1,17 +1,28 @@
-from DNADoor import DNADoor
+from DNANode import DNANode
 from DNAParser import *
 from panda3d.core import *
 
-class DNAFlatDoor(DNADoor):
+class DNAFlatDoor(DNANode):
     TAG = 'flat_door'
     PARENTS = ['wall']
 
     def __init__(self, code):
-        DNADoor.__init__(self, code)
+        DNANode.__init__(self, code)
 
         self.code = code
 
     def _makeNode(self, storage, parent):
-        pass # TODO
+        node = storage.findNode(self.code)
+        if node is None:
+            raise DNAError('DNAFlatDoor uses unknown code %s' % self.code)
+
+        np = node.copyTo(parent)
+        np.setScale(np.getTop(), (1, 1, 1)) # No net scale
+        np.setPos(0.5, 0, 0) # Centered within the wall
+
+        # Appear on top of wall:
+        np.setDepthOffset(50)
+
+        return np
 
 registerElement(DNAFlatDoor)
