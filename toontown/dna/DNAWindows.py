14b6e87fe8e8687d2dd3e4d761b72e8d30856dc7
Sam Edwards <CFSworks@gmail.com>
2014-04-08 02:04:53 -0600

    dna: Implement DNAWindows.

--- toontown/dna/DNAWindows.py
+++ toontown/dna/DNAWindows.py
@@ -13,6 +13,19 @@ class DNAWindows(DNANode):
         self.count = int(count)
 
     def _makeNode(self, storage, parent):
-        pass # TODO
+        node = storage.findNode(self.code)
+        if node is None:
+            raise DNAError('DNAWindows uses unknown code %s' % self.code)
+
+        windows = parent.attachNewNode('windows')
+        windows.setDepthOffset(50)
+
+        for i in xrange(self.count):
+            x = (i + 1)/(self.count + 1.0)
+
+            np = node.copyTo(windows)
+            np.setScale(np.getTop(), 1, 1, 1)
+            np.setX(x)
+            np.setZ(0.5)
 
 registerElement(DNAWindows)
