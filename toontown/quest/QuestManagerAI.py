ea5e0c0c16768046cb3d1a79eaba045e541131b4
Maverick Mosher <MMavipc@hostedyou.com>
2014-04-10 21:17:28 -0700

    QuestMgr: added better interation logic, can turn in quests, but not recv

--- toontown/quest/QuestManagerAI.py
+++ toontown/quest/QuestManagerAI.py
@@ -37,10 +37,56 @@ class QuestManagerAI:
     def toonRodeTrolleyFirstTime(self, toon):
         pass
 
+    def completeQuest(self, av, questId):
+        '''
+        Remove the quest from the player's quest inventory
+        This function exists because I think it may be necesary to do more logic here in the future, time will tell
+        '''
+        av.removeQuest(questId)
+
+    def giveReward(self, av, rewardId):
+        '''Give the player the reward'''
+        reward = Quests.getReward(rewardId)
+        if reward:
+            reward.sendRewardAI(av)
+
+    def npcGiveQuest(self, npc, av, questId, rewardId, toNpcId):
+        '''Have npc assign quest to av'''
+        av.addQuest((questId, npc.getDoId(), toNpcId, rewardId, 0), Quests.Quest2RemainingStepsDict[questId] == 1)
+        npc.assignQuest(av.getDoId(), questId, rewardId, toNpcId)
+
     def requestInteract(self, avId, npc):
+        '''Handle interactions between a player and an npc'''
         if simbase.air.doId2do.has_key(avId):
             av = simbase.air.doId2do[avId]
             quests = av.quests
+
+            #First, check if there are any quests to turn in
+            for quest in quests:
+                questDesc = quest
+                questId, fromNpcId, toNpcId, rewardId, toonProgress = questDesc
+                quest = Quests.getQuest(questId)
+                complete = quest.getCompletionStatus(av, questDesc, npc)
+                if complete == Quests.COMPLETE:
+                    #check if there is a next quest
+                    nextQuest = Quests.getNextQuest(questId, npc, av)
+                    if nextQuest == (Quests.NA, Quests.NA):
+                        rewardId = Quests.getAvatarRewardId(av, questId) #idk about this one, maybe a single quest can have different rewards?
+                        npc.completeQuest(avId, questId, rewardId)
+                        self.completeQuest(av, questId)
+                        self.giveReward(av, rewardId)
+                        return
+                    else:
+                        #gib quest
+                        questId = nextQuest[0]
+                        rewardId = Quests.getFinalRewardId(questId, 1)
+                        toNpcId = nextQuest[1]
+                        self.completeQuest(av, questId)
+                        self.npcGiveQuest(npc, av, questId, rewardId, toNpcId)
+
+            #stuff below this return is to-be rewritten
+            npc.rejectAvatar(npcId)
+            return
             numQuests = len(quests)
             if numQuests+1 > av.getQuestCarryLimit():
                 npc.rejectAvatar(avId)
@@ -66,6 +112,9 @@ class QuestManagerAI:
                 npc.rejectAvatarTierNotDone(avId)
                 return
             npc.presentQuestChoice(avId, offeredQuests)
+            return
+        #if nothing else, reject them
+        npc.rejectAvatar(avId)
 
     def avatarCancelled(self, avId):
         pass
@@ -73,11 +122,8 @@ class QuestManagerAI:
     def avatarChoseQuest(self, avId, npc, questId, rewardId, toNpcId):
         if simbase.air.doId2do.has_key(avId):
             av = simbase.air.doId2do[avId]
-            fromNpcId = npc.getDoId()
-            toonProgress = 0
             self.notify.debug("avatar chose quest %s"%str((questId, rewardId, toNpcId)))
-            av.addQuest((questId, fromNpcId, toNpcId, rewardId, toonProgress), Quests.Quest2RemainingStepsDict[questId] == 1)
-            npc.assignQuest(avId, questId, rewardId, toNpcId)
+            self.npcGiveQuest(npc, av, questId, rewardId, toNpcId)
 
     def avatarChoseTrack(self, avId, npc, pendingTrackQuest, trackId):
         pass
