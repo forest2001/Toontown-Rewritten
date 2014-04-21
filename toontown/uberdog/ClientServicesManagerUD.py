5f805e21db5c470b6cc3228e01714b0bc29aff4d
Harvir <harvir14@gmail.com>
2014-04-21 04:48:28 +0100

    CSMUD: By default, local DB accounts have full access.

--- toontown/uberdog/ClientServicesManagerUD.py
+++ toontown/uberdog/ClientServicesManagerUD.py
@@ -44,13 +44,13 @@ class LocalAccountDB:
             callback({'success': True,
                       'accountId': int(self.dbm[cookie]),
                       'databaseId': cookie,
-                      'adminAccess': 500})
+                      'adminAccess': 507})
         else:
             # Nope, let's return w/o account ID:
             callback({'success': True,
                       'accountId': 0,
                       'databaseId': cookie,
-                      'adminAccess': 500})
+                      'adminAccess': 507})
 
     def storeAccountID(self, databaseId, accountId, callback):
         self.dbm[databaseId] = str(accountId)
