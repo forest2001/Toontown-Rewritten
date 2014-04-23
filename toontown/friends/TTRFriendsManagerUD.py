c10db68e20355eadc12077774644c6d992534dbf
Harvir <harvir14@gmail.com>
2014-04-23 01:28:32 +0100

    friends: Add UpdateToonFieldFSM. A few minor fixes too.

--- toontown/friends/TTRFriendsManagerUD.py
+++ toontown/friends/TTRFriendsManagerUD.py
@@ -16,7 +16,7 @@ class GetToonDataFSM(FSM):
         self.mgr = mgr
         self.requesterId = requesterId
         self.avId = avId
-        self.callback
+        self.callback = callback
         
     def start(self):
         self.demand('QueryDB')
@@ -38,7 +38,69 @@ class GetToonDataFSM(FSM):
     def enterFailure(self, reason):
         self.mgr.notify.warning(reason)
         self.callback(success=False, None, None)
+        
+class UpdateToonFieldFSM(FSM):
+    """
+    A quick implementation to update a toon's fields in the
+    database and return a callback to the TTRFMUD.
+    """
+    
+    def __init__(self, mgr, requesterId, avId, callback):
+        FSM.__init__(self, 'UpdateToonDataFSM')
+        self.mgr = mgr
+        self.requesterId = requesterId
+        self.avId = avId
+        self.callback = callback
+        
+    def start(self, field, value):
+        self.field = field
+        self.value = value
+        self.demand('GetToonOnline')
+        
+    def enterGetToonOnline(self):
+        self.mgr.air.getActivated(avId, self.__toonOnlineResp)
+        
+    def __toonOnlineResp(self, avId, activated):
+        if self.state != 'GetToonOnline':
+            self.demand('Failure', 'Received __toonOnlineResp while not in GetToonOnline state.')
+            return
+        self.online = activated
+        self.demand('UpdateDB')
+       
+    def enterUpdateDB(self):
+        if self.online:
+            dg = self.mgr.air.dclassesByName['DistributedToonUD'].aiFormatUpdate(
+                    self.field, self.avId, self.avId, self.mgr.air.ourChannel, [self.value]
+                )
+            self.mgr.air.send(dg)
+        else:
+            self.mgr.air.dbInterface.updateObject(
+                self.mgr.air.dbId,
+                self.avId,
+                self.mgr.air.dclassesByName['DistributedToonUD'],
+                { self.field : [self.value] }
+            )
+        self.demand('Finished')
+        
+    def enterFinished(self):
+        self.callback(success=True)
+    
+    def enterFailure(self, reason):
+        self.mgr.notify.warning(reason)
+        self.callback(success=False)
+        
 
 class TTRFriendsManagerUD(DistributedObjectGlobalUD):
+    """
+    The Toontown Rewritten Friends Manager UberDOG, or TTRFMUD for short.
+    
+    This object is responsible for all requests related to global friends, such as
+    friends coming online, friends going offline, fetching a friends data etc.
+    """
+    
     notify = directNotify.newCategory('TTRFriendsManagerUD')
-    pass
+    
+    def __init__(self, air):
+        DistributedObjectGlobalUD.__init__(self, air)
+        # requesterId : fsm
+        self.fsms = {}
