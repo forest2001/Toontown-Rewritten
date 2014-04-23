87404dc999f62ffbf1c2db95a6c1eab2c59fb855
Harvir <harvir14@gmail.com>
2014-04-23 02:27:20 +0100

    friends: Implement removeFriend on UD. Sad times.

--- toontown/friends/TTRFriendsManagerUD.py
+++ toontown/friends/TTRFriendsManagerUD.py
@@ -83,7 +83,7 @@ class UpdateToonFieldFSM(FSM):
         self.demand('Finished')
         
     def enterFinished(self):
-        self.callback(success=True)
+        self.callback(success=True, requesterId=self.requesterId, online=self.online)
     
     def enterFailure(self, reason):
         self.mgr.notify.warning(reason)
@@ -104,3 +104,69 @@ class TTRFriendsManagerUD(DistributedObjectGlobalUD):
         DistributedObjectGlobalUD.__init__(self, air)
         # requesterId : fsm
         self.fsms = {}
+        
+    def deleteFSM(self, requesterId):
+        fsm = self.fsms.get(requesterId)
+        if not fsm:
+            # Just print debug incase we ever have issues.
+            self.notify.debug('%d tried to delete non-existent FSM!' % requesterId)
+            return
+        if fsm.state != 'Off':
+            fsm.demand('Failure')
+        del self.fsms[requesterId]
+        
+    def removeFriend(self, avId):
+        requesterId = self.air.getAvatarIdFromSender()
+        if requesterId in self.fsms:
+            # Looks like the requester already has an FSM running. In the future we
+            # may want to handle this, but for now just ignore it.
+            return
+        # We need to get the friends list of the requester.
+        fsm = GetToonFieldsFSM(self, requesterId, requesterId, functools.partial(self.__rfGotToonFields, avId=avId))
+        fsm.start()
+        self.fsms[requsterId] = fsm
+        
+    def __rfGotToonFields(self, success, requesterId, fields, avId=None, final=False):
+        if not (success and avId):
+            # Something went wrong... Delete the FSM and stop.
+            self.deleteFSM(requesterId)
+            return
+        if fields['ID'] not in [requesterId, avId]:
+            # Wtf? We got a db response for a toon that we didn't want
+            # to edit! DEFCON 5!
+            self.deleteFSM(requesterId)
+            self.notify.warning('TTRFMUD.__rfGotToonFields received wrong toon fields from db, requesterId=%d' % requesterId)
+            return
+        friendsList = fields['setFriendsList'][0]
+        for index, friendId in enumerate(friendsList):
+            if friendId[0] == avId:
+                del friendsList[index]
+                break
+        self.deleteFSM(requesterId)
+        fsm = UpdateToonFieldFSM(self, requesterId, requesterId, functools.partial(self.__removeFriendCallback, avId=avId, final=final))
+        fsm.start('setFriendsList', friendsList)
+        self.fsms[requesterId] = fsm
+        
+    def __removeFriendCallback(self, success, requesterId, online=False, avId=None, final=False):
+        if not (success and avId):
+            # Something went wrong... Delete the FSM and stop.
+            self.deleteFSM(requesterId)
+            return
+        if not final:
+            # Toon was deleted from the friends list successfully! Now we need to modify
+            # the other toons friends list...
+            self.deleteFSM(requesterId)
+            fsm = GetToonFieldsFSM(self, requesterId, avId, functools.partial(self.__rfGotToonFields, avId=avId, final=True))
+            fsm.start()
+            self.fsms[requesterId] = fsm
+        else:
+            # We're finished with everything!
+            if online:
+                # Lets notify their friend that they went bye bye!
+                # Sad times for this toon. :(...
+                dg = self.air.dclassesByName['DistributedToonUD'].aiFormatUpdate(
+                    'friendsNotify', avId, avId, self.air.ourChannel, [requesterId, 1]
+                )
+                self.air.send(dg)
+            # Clean up our mess and we are finished!
+            self.deleteFSM(requesterId)
