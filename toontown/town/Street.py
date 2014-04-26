2b4c2ccc26daebfb847b59fd232ef02394bfe8ef
Joey Z <joey1998212@gmail.com>
2014-04-25 20:25:52 -0400

    town: Import NametagGlobals

--- toontown/town/Street.py
+++ toontown/town/Street.py
@@ -19,7 +19,6 @@ from toontown.toon.Toon import teleportDebug
 from toontown.estate import HouseGlobals
 from toontown.toonbase import TTLocalizer
 from direct.interval.IntervalGlobal import *
-from otp.nametag.NametagConstants import *
 from otp.nametag import NametagGlobals
 visualizeZones = base.config.GetBool('visualize-zones', 0)
 
@@ -114,7 +113,7 @@ class Street(BattlePlace.BattlePlace):
         base.localAvatar.setGeom(self.loader.geom)
         base.localAvatar.setOnLevelGround(1)
         self._telemLimiter = TLGatherAllAvs('Street', RotationLimitToH)
-        NametagGlobals.setMasterArrowsOn(arrowsOn) #TODO: fix me cfsworks
+        NametagGlobals.setMasterArrowsOn(arrowsOn)
 
         def __lightDecorationOn__():
             geom = base.cr.playGame.getPlace().loader.geom
@@ -186,7 +185,7 @@ class Street(BattlePlace.BattlePlace):
                 light.reparentTo(hidden)
 
         newsManager = base.cr.newsManager
-        NametagGlobals.setMasterArrowsOn(0) #TODO: cfsworks fix me plx
+        NametagGlobals.setMasterArrowsOn(0)
         self.loader.hood.stopSky()
         self.loader.music.stop()
         base.localAvatar.setGeom(render)
