267dd61226a7ed344a90c8833b53ad81f7fe3924
Sam Edwards <CFSworks@gmail.com>
2014-04-26 17:36:59 -0600

    dna: Change DNAUtil.getVisGroups to utilize getChildrenOfType.

--- toontown/dna/DNAUtil.py
+++ toontown/dna/DNAUtil.py
@@ -2,25 +2,19 @@ from DNAVisGroup import DNAVisGroup
 import re
 
 
-def getVisGroups(root):
-    visGroups = []
-    r_getVisGroups(root, visGroups)
-    return visGroups
-def r_getVisGroups(root, list):
-    for child in root.children:
-        if isinstance(child, DNAVisGroup):
-            list.append(child)
-        r_getVisGroups(child, list)
-
 def getChildrenOfType(root, type):
     list = []
     r_getChildrenOfType(root, type, list)
     return list
+
 def r_getChildrenOfType(root, type, list):
     for child in root.children:
         if isinstance(child, type):
             list.append(child)
         r_getVisGroups(child, list)
+
+def getVisGroups(root):
+    return getChildrenOfType(DNAVisGroup)
 
 
 INDEX_REGEX = re.compile('tb([0-9]+):')
