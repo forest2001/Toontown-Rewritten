c06d0c486a27a6cbf528492e493852a7bdd66139
Anthony Castelli <neueanthony@me.com>
2014-05-10 16:20:20 -0700

    lawbot: DA Doors no longer turn you into a suit
    Disney shit

--- toontown/hood/LawbotHQAI.py
+++ toontown/hood/LawbotHQAI.py
@@ -17,19 +17,22 @@ class LawbotHQAI(CogHoodAI):
         self.createZone()
         
     def createDoor(self):
-        daInteriorDoor = DistributedCogHQDoorAI(self.air, 0, DoorTypes.INT_COGHQ, self.HOOD)
-        daExteriorDoor = DistributedCogHQDoorAI(self.air, 0, DoorTypes.EXT_COGHQ, ToontownGlobals.LawbotOfficeExt)
+        # DA Offices
+        daInteriorDoor = DistributedCogHQDoorAI(self.air, 0, DoorTypes.EXT_COGHQ, self.HOOD, doorIndex=0)
+        daExteriorDoor = DistributedCogHQDoorAI(self.air, 0, DoorTypes.INT_COGHQ, ToontownGlobals.LawbotOfficeExt, doorIndex=0, lockValue=FADoorCodes.UNLOCKED)
         daExteriorDoor.setOtherDoor(daInteriorDoor)
         daExteriorDoor.zoneId = self.HOOD
         daExteriorDoor.generateWithRequired(self.HOOD)
         daExteriorDoor.sendUpdate('setDoorIndex', [0])
         self.doors.append(daExteriorDoor)
+
         daInteriorDoor.setOtherDoor(daExteriorDoor)
         daInteriorDoor.zoneId = ToontownGlobals.LawbotOfficeExt
         daInteriorDoor.generateWithRequired(ToontownGlobals.LawbotOfficeExt)
         daInteriorDoor.sendUpdate('setDoorIndex', [0])
         self.doors.append(daInteriorDoor)
         
+        # CJ Lobby
         interiorDoor = DistributedCogHQDoorAI(self.air, 0, DoorTypes.INT_COGHQ, self.HOOD, doorIndex=0)
         exteriorDoor = DistributedCogHQDoorAI(self.air, 0, DoorTypes.EXT_COGHQ, ToontownGlobals.LawbotLobby, doorIndex=1, lockValue=FADoorCodes.LB_DISGUISE_INCOMPLETE)
         exteriorDoor.setOtherDoor(interiorDoor)
@@ -37,6 +40,7 @@ class LawbotHQAI(CogHoodAI):
         exteriorDoor.generateWithRequired(self.HOOD)
         exteriorDoor.sendUpdate('setDoorIndex', [1])
         self.doors.append(exteriorDoor)
+
         interiorDoor.setOtherDoor(exteriorDoor)
         interiorDoor.zoneId = ToontownGlobals.LawbotLobby
         interiorDoor.generateWithRequired(ToontownGlobals.LawbotLobby)
@@ -59,7 +63,7 @@ class LawbotHQAI(CogHoodAI):
         # Create Suit Planners in the cog playground
         self.createSuitPlanner(self.HOOD)
         
-        # Create mint elevators.
+        # Create DA Office Elevators.
         mins = ToontownGlobals.FactoryLaffMinimums[2]
         self.officeA = self.createElevator(DistributedLawOfficeElevatorExtAI, self.air.lawOfficeMgr, ToontownGlobals.LawbotOfficeExt, ToontownGlobals.LawbotOfficeInt, 0, minLaff=mins[0])
         self.officeB = self.createElevator(DistributedLawOfficeElevatorExtAI, self.air.lawOfficeMgr, ToontownGlobals.LawbotOfficeExt, ToontownGlobals.LawbotOfficeInt, 1, minLaff=mins[1])
