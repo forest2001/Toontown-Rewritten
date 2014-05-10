29f07af1e7e58b1165c24360b38bd075473e4686
Aidan <aidan.noll@gmail.com>
2014-05-10 16:38:43 -0400

    coghq: add FoodBeltAI

--- toontown/coghq/DistributedFoodBeltAI.py
+++ toontown/coghq/DistributedFoodBeltAI.py
@@ -1,15 +1,68 @@
+from direct.distributed import DistributedObjectAI
+from direct.fsm import FSM
 from direct.directnotify import DirectNotifyGlobal
-from direct.distributed.DistributedObjectAI import DistributedObjectAI
+from toontown.coghq import FoodBeltBase
 
-class DistributedFoodBeltAI(DistributedObjectAI):
-    notify = DirectNotifyGlobal.directNotify.newCategory("DistributedFoodBeltAI")
+class DistributedFoodBeltAI(DistributedObjectAI.DistributedObjectAI, FSM.FSM, FoodBeltBase.FoodBeltBase):
+    notify = DirectNotifyGlobal.directNotify.newCategory('DistributedFoodBeltAI')
 
-    def setBossCogId(self, todo0):
+    def __init__(self, air, boss, index):
+        DistributedObjectAI.DistributedObjectAI.__init__(self, air)
+        FSM.FSM.__init__(self, 'DistributedFoodBeltAI')
+        self.boss = boss
+        self.index = index
+
+    def delete(self):
+        DistributedObjectAI.DistributedObjectAI.delete(self)
+
+    def getBossCogId(self):
+        return self.boss.doId
+
+    def getIndex(self):
+        return self.index
+
+    def setState(self, state):
+        self.request(state)
+
+    def d_setState(self, state):
+        newState = state
+        if state == 'On':
+            newState = 'N'
+        elif state == 'Off':
+            newState = 'F'
+        elif state == 'Inactive':
+            newState = 'I'
+        elif state == 'Toonup':
+            newState = 'T'
+        self.sendUpdate('setState', [newState])
+
+    def b_setState(self, state):
+        self.request(state)
+        self.d_setState(state)
+
+    def turnOn(self):
+        self.b_setState('On')
+
+    def goInactive(self):
+        self.b_setState('Inactive')
+
+    def goToonup(self):
+        self.b_setState('Toonup')
+
+    def enterOn(self):
         pass
 
-    def setIndex(self, todo0):
+    def exitOn(slef):
         pass
 
-    def setState(self, todo0):
+    def enterOff(self):
         pass
 
+    def exitOff(self):
+        pass
+
+    def enterInactive(self):
+        pass
+
+    def exitInactive(slef):
+        pass
