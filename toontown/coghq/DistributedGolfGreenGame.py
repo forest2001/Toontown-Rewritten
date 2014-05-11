a479b308ee80d1f1a193a799c5cb441ed12d2f03
Aidan <aidan.noll@gmail.com>
2014-05-11 03:12:41 -0400

    coghq: fix cog golf crashes

--- toontown/coghq/DistributedGolfGreenGame.py
+++ toontown/coghq/DistributedGolfGreenGame.py
@@ -553,7 +553,8 @@ class DistributedGolfGreenGame(BattleBlocker.BattleBlocker):
             sprite.delete()
 
         self.sprites = []
-        self.spriteNode.hide()
+        if not self.spriteNode.isEmpty():
+            self.spriteNode.hide()
         self.controlSprite = None
         self.running = 0
         self.timerLabel.hide()
