d28e5c78429c1c8d467cf442719a522dd14c8cce
Harvir <harvir14@gmail.com>
2014-05-18 03:47:08 +0100

    tutorial: Reset max hp and zero out inventory when arriving.

--- toontown/tutorial/TutorialManagerAI.py
+++ toontown/tutorial/TutorialManagerAI.py
@@ -17,15 +17,15 @@ class TZoneStruct:
     street = 0
     shop = 0
     hq = 0
-    
-    
+
+
 class ToontorialBuildingAI:
     def __init__(self, air, street, interior, npcId):
-    
+
         self.air = air
-        
+
         self.interior = DistributedTutorialInteriorAI(self.air, interior, npcId)
-        
+
         self.interior.generateWithRequired(interior)
         self.door0 = DistributedDoorAI.DistributedDoorAI(self.air, 2, DoorTypes.EXT_STANDARD, doorIndex=0)
         self.insideDoor0 = DistributedDoorAI.DistributedDoorAI(self.air, 0, DoorTypes.INT_STANDARD, doorIndex=0)
@@ -37,66 +37,66 @@ class ToontorialBuildingAI:
         self.door0.sendUpdate('setDoorIndex', [self.door0.getDoorIndex()])
         self.insideDoor0.generateWithRequired(interior)
         self.insideDoor0.sendUpdate('setDoorIndex', [self.insideDoor0.getDoorIndex()])
-        
+
     def cleanup(self):
         self.door0.requestDelete()
         self.insideDoor0.requestDelete()
         self.interior.requestDelete()
 
-    
+
 class TutorialFSM(FSM):
     def __init__(self, air, zones, avId):
         FSM.__init__(self, 'TutorialFSM')
         self.avId = avId
         self.zones = zones
         self.air = air
-        
+
         npcDesc = NPCToons.NPCToonDict.get(20000)
         self.tom = NPCToons.createNPC(self.air, 20000, npcDesc, self.zones.shop, 0)
         self.tom.setTutorial(1)
-        
+
         npcDesc = NPCToons.NPCToonDict.get(20002)
         self.harry = NPCToons.createNPC(self.air, 20002, npcDesc, self.zones.hq, 0)
         self.harry.setTutorial(1)
-                
+
         self.building = ToontorialBuildingAI(self.air, zones.street, zones.shop, self.tom.getDoId())
         self.hq = HQBuildingAI(self.air, zones.street, zones.hq, 1)
-        
+
         self.forceTransition('Introduction')
-        
-    def enterIntroduction(self):        
+
+    def enterIntroduction(self):
         self.building.insideDoor0.setDoorLock(FADoorCodes.TALK_TO_TOM)
-        
+
     def exitIntroduction(self):
         self.building.insideDoor0.setDoorLock(FADoorCodes.UNLOCKED)
-        
+
     def enterBattle(self):
         self.suit = DistributedTutorialSuitAI(self.air)
         self.suit.generateWithRequired(self.zones.street)
         self.building.door0.setDoorLock(FADoorCodes.DEFEAT_FLUNKY_TOM)
         self.hq.door0.setDoorLock(FADoorCodes.DEFEAT_FLUNKY_HQ)
-        
+
     def exitBattle(self):
         if self.suit:
             self.suit.requestDelete()
-                
+
     def enterHQ(self):
         self.building.door0.setDoorLock(FADoorCodes.TALK_TO_HQ_TOM)
         self.hq.door0.setDoorLock(FADoorCodes.UNLOCKED)
         self.hq.insideDoor0.setDoorLock(FADoorCodes.TALK_TO_HQ)
         self.hq.insideDoor1.setDoorLock(FADoorCodes.TALK_TO_HQ)
-        
+
     def enterTunnel(self):
         npcDesc = NPCToons.NPCToonDict.get(20001)
         self.flippy = NPCToons.createNPC(self.air, 20001, npcDesc, self.zones.street, 0)
-        
+
         self.hq.insideDoor1.setDoorLock(FADoorCodes.UNLOCKED)
         self.hq.door1.setDoorLock(FADoorCodes.GO_TO_PLAYGROUND)
         self.hq.insideDoor0.setDoorLock(FADoorCodes.WRONG_DOOR_HQ)
-        
+
     def exitTunnel(self):
         self.flippy.requestDelete()
-        
+
     def enterCleanUp(self):
         #deallocate all the zones
         self.building.cleanup()
@@ -124,12 +124,12 @@ class TutorialManagerAI(DistributedObjectAI):
         zones.street = self.air.allocateZone()
         zones.shop = self.air.allocateZone()
         zones.hq = self.air.allocateZone()
-        
+
         self.avId2fsm[avId] = TutorialFSM(self.air, zones, avId)
-        
+
         self.acceptOnce(self.air.getAvatarExitEvent(avId), self.unexpectedExit, extraArgs=[avId])
-        
-        self.d_enterTutorial(avId, zones.street, zones.street, zones.shop, zones.hq) #hackfix lololol        
+
+        self.d_enterTutorial(avId, zones.street, zones.street, zones.shop, zones.hq) #hackfix lololol
 
     def unexpectedExit(self, avId):
         fsm = self.avId2fsm.get(avId)
@@ -181,7 +181,9 @@ class TutorialManagerAI(DistributedObjectAI):
         av.b_setQuestHistory([])
         av.b_setRewardHistory(0, [])
         av.b_setHp(15)
+        av.b_setMaxHp(15)
 
+        av.inventory.zeroInv()
         if av.inventory.numItem(ToontownBattleGlobals.THROW_TRACK, 0) == 0:
             av.inventory.addItem(ToontownBattleGlobals.THROW_TRACK, 0)
         if av.inventory.numItem(ToontownBattleGlobals.SQUIRT_TRACK, 0) == 0:
