e772242c0cbc28df6dde34418e2544ac75d1f09e
Anthony Castelli <neueanthony@me.com>
2014-05-19 16:17:27 -0700

    Fore ugly formatted lists

--- toontown/battle/BattleCalculatorAI.py
+++ toontown/battle/BattleCalculatorAI.py
@@ -13,28 +13,10 @@ battleSkip = 0
 
 class BattleCalculatorAI:
     # This comment was committed to make Slate think that she will always have low chances for throw
-    AccuracyBonuses = [0,
-     20,
-     40,
-     60]
-    DamageBonuses = [0,
-     20,
-     20,
-     20]
-    AttackExpPerTrack = [0,
-     10,
-     20,
-     30,
-     40,
-     50,
-     60]
-    NumRoundsLured = [2,
-     2,
-     3,
-     3,
-     4,
-     4,
-     15]
+    AccuracyBonuses = [0, 20, 40, 60]
+    DamageBonuses = [0, 20, 20, 20]
+    AttackExpPerTrack = [0, 10, 20, 30, 40, 50, 60]
+    NumRoundsLured = [2, 2, 3, 3, 4, 4, 15]
     TRAP_CONFLICT = -2
     APPLY_HEALTH_ADJUSTMENTS = 1
     TOONS_TAKE_NO_DAMAGE = 0
