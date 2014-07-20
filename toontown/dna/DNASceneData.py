2650cf6eded9ede8ceb92adfbbee8af592d7b267
Sam Edwards <CFSworks@gmail.com>
2014-07-20 04:58:42 -0700

    dna: Replace slow BFS-based suit path search with a modified distance-vector routing tree.
    This makes suit pathing approximately 50x faster (at the expense of
    more AI startup time), eliminating the the single largest cause of AI lag.

--- toontown/dna/DNASceneData.py
+++ toontown/dna/DNASceneData.py
@@ -2,17 +2,72 @@ from DNAParser import *
 import DNAStoreSuitPoint
 from collections import deque
 
-class DNASuitGraph:
+class DNASuitGraphLink(object):
+    unboundedDistance = None
+    unboundedNext = None
+    boundedDistance = None
+    boundedNext = None
+
+class DNASuitGraphVertex(object):
+    def __init__(self, point, traversable):
+        self.visitIndex = 0
+
+        self.paths = {}
+        self.neighbors = []
+
+        self.point = point
+        self.traversable = traversable
+
+    def processLink(self, neighbor, distance, destination, unbounded=False):
+        """Process a link announcement from a neighbor:
+
+        Neighbor `neighbor` is telling us that it has a `distance` cost link to
+        `destination`.
+        """
+
+        unbounded = (self.visitIndex > 0) or unbounded
+
+        self.visitIndex += 1
+
+        link = self.paths.setdefault(destination, DNASuitGraphLink())
+        existingDistance = link.unboundedDistance if unbounded else link.boundedDistance
+
+        if existingDistance is None or distance < existingDistance:
+            if unbounded:
+                link.unboundedDistance = distance
+                link.unboundedNext = neighbor
+            else:
+                link.boundedDistance = distance
+                link.boundedNext = neighbor
+
+            if self.traversable:
+                # Duplicated from announceLink to prevent deep recursion.
+                for neighbor in self.neighbors:
+                    neighbor.processLink(self, distance, destination, unbounded)
+
+        self.visitIndex -= 1
+
+    def announceLink(self, distance, destination, unbounded):
+        for neighbor in self.neighbors:
+            neighbor.processLink(self, distance, destination, unbounded)
+
+class DNASuitGraph(object):
     def __init__(self, points, edges):
         self.points = points
         self.edges = edges
 
         self._pointId2point = {}
+        self._point2vertex = {}
         self._point2outboundEdges = {}
         self._point2inboundEdges = {}
 
         for point in points:
             self._pointId2point[point.id] = point
+
+            traversable = point.type in (DNAStoreSuitPoint.STREETPOINT,
+                                         DNAStoreSuitPoint.COGHQINPOINT,
+                                         DNAStoreSuitPoint.COGHQOUTPOINT)
+            self._point2vertex[point] = DNASuitGraphVertex(point, traversable)
 
         for edge in edges:
             try:
@@ -23,6 +78,11 @@ class DNASuitGraph:
 
             self._point2outboundEdges.setdefault(a, []).append(edge)
             self._point2inboundEdges.setdefault(b, []).append(edge)
+
+            self._point2vertex[b].neighbors.append(self._point2vertex[a])
+
+        for vertex in self._point2vertex.values():
+            vertex.announceLink(1, vertex, False)
 
     def getEdgeEndpoints(self, edge):
         return self._pointId2point[edge.a], self._pointId2point[edge.b]
@@ -44,47 +104,33 @@ class DNASuitGraph:
         return self.getEdgeZone(edges[0])
 
     def getSuitPath(self, startPoint, endPoint, minPathLen, maxPathLen):
-        # Performs a BFS in order to find a path from startPoint to endPoint,
-        # the minimum length will be minPathLen, and the maximum will be
-        # maxPathLen. N.B. these values indicate the length in edges, not
-        # vertices, so the returned list will be:
-        # minPathLen+1 <= len(list) <= maxPathLen+1
+        minpl = minPathLen
+        maxpl = maxPathLen
+        start = self._point2vertex[startPoint]
+        end = self._point2vertex[endPoint]
 
-        # The queue of paths to consider:
-        # The format is a tuple: (prevPath, depth, point)
-        pathDeque = deque()
-        pathDeque.append((None, 0, startPoint))
-        while pathDeque:
-            path = pathDeque.popleft()
-            prevPath, depth, point = path
+        at = start
+        path = [startPoint]
 
-            newDepth = depth + 1
-            if newDepth > maxPathLen:
-                # This path has grown too long, prune it.
-                continue
+        while at != end or minPathLen > 0:
+            link = at.paths.get(end)
 
-            for adj in self.getAdjacentPoints(point):
-                if adj == endPoint and newDepth >= minPathLen:
-                    # Hey, we found the end! Let's return it:
-                    points = deque()
-                    points.appendleft(adj)
-                    while path:
-                        points.appendleft(path[-1])
-                        path, _, _ = path
-                    return list(points)
+            if link is None:
+                return None # Failure!
 
-                # We're not at the end yet... Let's see if we can traverse this
-                # point:
-                if adj.type not in (DNAStoreSuitPoint.STREETPOINT,
-                                    DNAStoreSuitPoint.COGHQINPOINT,
-                                    DNAStoreSuitPoint.COGHQOUTPOINT):
-                    # This is some other special point that we cannot walk
-                    # across.
-                    continue
+            if link.boundedDistance is not None and minPathLen <= link.boundedDistance <= maxPathLen:
+                at = link.boundedNext
+            elif link.unboundedDistance is not None and link.unboundedDistance <= maxPathLen:
+                at = link.unboundedNext
+            else:
+                # No path exists!
+                return None
 
-                # Append this point to the paths we are considering:
-                pathDeque.append((path, newDepth, adj))
+            path.append(at.point)
+            minPathLen -= 1
+            maxPathLen -= 1
 
+        return path
 
     def getAdjacentPoints(self, point):
         return [self.getPointFromIndex(edge.b) for edge in self.getEdgesFrom(point)]
