ee96def327147d4d320577a8d1a84670f2dc0bb4
Harvir <harvir14@gmail.com>
2014-07-30 01:36:18 +0100

    dg: Add butterflies and flower back.

--- toontown/hood/DGHoodAI.py
+++ toontown/hood/DGHoodAI.py
@@ -10,5 +10,19 @@ class DGHoodAI(SZHoodAI):
 
     def createZone(self):
         SZHoodAI.createZone(self)
-
+        self.butterflies = []
         self.spawnObjects()
+
+        self.flower = DistributedDGFlowerAI(self.air)
+        self.flower.generateWithRequired(self.HOOD)
+        self.createButterflies()
+
+    def createButterflies(self):
+        playground = ButterflyGlobals.DG
+        for area in range(ButterflyGlobals.NUM_BUTTERFLY_AREAS[playground]):
+            for b in range(ButterflyGlobals.NUM_BUTTERFLIES[playground]):
+                butterfly = DistributedButterflyAI(self.air)
+                butterfly.setArea(playground, area)
+                butterfly.setState(0, 0, 0, 1, 1)
+                butterfly.generateWithRequired(self.HOOD)
+                self.butterflies.append(butterfly)
