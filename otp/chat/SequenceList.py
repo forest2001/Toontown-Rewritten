1263091244e03769b4fb03d9fbc07cec281b6480
Cosmos <cosmified@gmail.com>
2014-08-02 16:05:52 -0600

    otp: "duck:my life" indeed.

--- otp/chat/SequenceList.py
+++ otp/chat/SequenceList.py
@@ -2,7 +2,7 @@ class SequenceList:
 
     def __init__(self, wordlist):
         self.list = {}
-        for line in wordlist.split():
+        for line in wordlist.split('\r\n'):
             split = line.split(':')
             self.list[split[0].lower()] = [word.rstrip('\r\n').lower() for word in split[1].split(',')]
             
