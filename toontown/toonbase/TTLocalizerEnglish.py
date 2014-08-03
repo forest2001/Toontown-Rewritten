0554519e74d8e15a7a2cb47e38534e17ca49ba45
Anthony Castelli <neueanthony@me.com>
2014-08-03 11:27:55 -0700

    Revert "last-seen: Redo the relative date stuff because it bugged me"
    This reverts commit 411dd98818598a8723535e41e3faed18cd52940e.

--- toontown/toonbase/TTLocalizerEnglish.py
+++ toontown/toonbase/TTLocalizerEnglish.py
@@ -1922,60 +1922,38 @@ AvatarDetailPanelOnline = 'District: %(district)s\nLocation: %(location)s'
 AvatarDetailPanelOnlinePlayer = 'District: %(district)s\nLocation: %(location)s\nPlayer: %(player)s'
 AvatarDetailPanelOffline = 'District: offline\nLocation: offline\nLast Seen: %(last_seen)s'
 
-DATEFORMAT = '%Y-%m-%d %H:%M:%S'
-import time, datetime
-
+import time
 def getLastSeenString(timestamp):
-    ts = datetime.datetime.fromtimestamp(timestamp).strftime(DATEFORMAT)
-    date = datetime.datetime.strptime(ts, DATEFORMAT)
-
-    current_datetime = datetime.datetime.now()
-    delta = str(current_datetime - date)
-
-    if delta.find(',') > 0:
-        days, hours = delta.split(',')
-        days = int(days.split()[0].strip())
-        hours, minutes, seconds = hours.split(':')[0:3]
-    else:
-        hours, minutes, seconds = delta.split(':')[0:3]
-        days = 0
-
-    days, hours, minutes = int(days), int(hours), int(minutes)
-    datelets =[]
-    years, months, xdays = None, None, None
-    plural = lambda x: 's' if x != 1 else ''
-
-    if days >= 365:
-        years = int(days / 365)
-        datelets.append('%d year%s' % (years, plural(years)))
-        days = days % 365
-
-    if days >= 30 and days < 365:
-        months = int(days / 30)
-        datelets.append('%d month%s' % (months, plural(months)))        
-        days = days % 30
-
-    if days >= 7:
-        xdays = days
-        datelets.append('%d week%s' % (xdays, plural(xdays)))
-
-    if not years and days > 0 and days < 30:
-        xdays = days
-        datelets.append('%d day%s' % (xdays, plural(xdays)))   
-
-    if not (months or years) and hours != 0:
-        datelets.append('%d hour%s' % (hours, plural(hours))) 
-
-    if not (xdays or months or years):
-        datelets.append('%d minute%s' % (minutes, plural(minutes)))
-
-    # This is where it turns to shit
-    if (minutes == 0 and xdays == 0 and months == 0 and years == 0) and float(seconds) == 0:
-        return "Just now"
-    elif (minutes == 0 and xdays == 0 and months == 0 and years == 0) and float(seconds) < 60:
+    # use int() to round down
+    seconds_passed = int(time.time()) - int(timestamp)
+    if timestamp == 0:
+        # This is the default. It means the db never had an update for lastSeen.
+        return "Never"
+    elif seconds_passed < 60:
         return "Less than a minute ago"
-
-    return ', '.join(datelets) + ' ago.' # Would return something like "1 hour and 15 minutes ago"
+    elif seconds_passed < 60*2: # less than 2 minutes
+        return "1 minute ago"
+    elif seconds_passed < 60*60: # less than 1 hour
+        return "%d minutes ago" % int(seconds_passed/60)
+    elif seconds_passed < 60*60*2: # less than 2 hours
+        return "1 hour ago"
+    elif seconds_passed < 60*60*24: # less than 1 day
+        return "%d hours ago" % int(seconds_passed/(60*60))
+    elif seconds_passed < 60*60*24*2: # less than 2 days
+        return "1 day ago"
+    # optional: at this stage we could do weeks, but seems pointless.
+    # we also now pretend that each month always has 30 days.
+    elif seconds_passed < 60*60*24*30: # less than a month
+        return "%d days ago" % int(seconds_passed/(60*60*24))
+    elif seconds_passed < 60*60*24*30*2: # less than 2 months
+        return "1 month ago"
+    # assume 1 year = 365 days (ignoring .25 / leap years)
+    elif seconds_passed < 60*60*24*365: # less than a year
+        return "%d months ago" % int(seconds_passed/(60*60*24*30))
+    elif seconds_passed < 60*60*24*365*2: # less than 2 years
+        return "1 year ago"
+    else:
+        return "A very long time ago... :("
 
 AvatarShowPlayer = 'Show Player'
 OfflineLocation = 'Offline'
