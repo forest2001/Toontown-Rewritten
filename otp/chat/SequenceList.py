f5f033edc8876a798d3603e14d0c2c060714a3a7
Joey Z <joey19982@yahoo.com>
2014-08-07 17:41:35 -0400

    chat: Don't break the uberdog if there is a new line

--- otp/chat/SequenceList.py
+++ otp/chat/SequenceList.py
@@ -3,6 +3,8 @@ class SequenceList:
     def __init__(self, wordlist):
         self.list = {}
         for line in wordlist.split('\r\n'):
+            if line is '':
+                continue
             split = line.split(':')
             self.list[split[0].lower()] = [word.rstrip('\r\n').lower() for word in split[1].split(',')]
             
